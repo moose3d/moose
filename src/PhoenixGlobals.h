55fb0c801b6d4d12319330138da0d8978bc9d881
entity <entity@cc4fdaca-9534-0410-bcb6-82ce2780185d>
2007-07-07 12:28:10 +0000

    first global constant
    git-svn-id: svn://localhost/phoenix/trunk@57 cc4fdaca-9534-0410-bcb6-82ce2780185d

---
+++ src/PhoenixGlobals.h
@@ -0,0 +1,13 @@
+#ifndef __PhoenixGlobals_h__
+#define __PhoenixGlobals_h__
+/////////////////////////////////////////////////////////////////
+namespace Phoenix
+{
+  namespace Graphics
+  {
+    /// Maximum number of characters in a font.
+    const unsigned int MAX_FONT_CHARACTERS = 256;
+  }
+}
+/////////////////////////////////////////////////////////////////
+#endif
