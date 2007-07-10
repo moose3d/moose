faaa188c35333ccf5745be4a2d762dc181d7924c
entity <entity@cc4fdaca-9534-0410-bcb6-82ce2780185d>
2007-07-10 14:07:58 +0000

    addedtests
    git-svn-id: svn://localhost/phoenix/trunk@102 cc4fdaca-9534-0410-bcb6-82ce2780185d

--- tests/PhoenixAVLTreeTest.cpp
+++ tests/PhoenixAVLTreeTest.cpp
@@ -4,6 +4,8 @@
 #include <math.h>
 #include <assert.h>
 using namespace Phoenix::Core::AVLTree;
+using namespace std;
+const int MAX_TEST_NODES = 5;
 #define IntStringKeyPair KeyValue<std::string>
 /////////////////////////////////////////////////////////////////
 class Handler 
@@ -20,6 +22,7 @@ public:
   }
   void Handle( CAVLTreeNode< IntStringKeyPair > &node )
   {
+    std::cerr << node.GetKeyValue().GetKey() << std::endl;
     if ( first )
     {
       first = 0;
@@ -27,15 +30,41 @@ public:
     } 
     else
     {
-      assert( (previous + 1) == node.GetKeyValue().GetKey() && "Invalid key order!");
       previous = node.GetKeyValue().GetKey();
     }
     if ( node.GetHeight() > maxheight )  maxheight = node.GetHeight();
   }
 };
-TEST( AVLTree )
+
+/////////////////////////////////////////////////////////////////
+TEST( AVLTreeInsertLinearReverse )
 {
-  const int MAX_TEST_NODES = 2000000;
+  
+  CAVLTree<IntStringKeyPair > *pTree = new  CAVLTree<IntStringKeyPair >(); 
+  std::cerr << "inserting, please wait: " << std::endl;
+  IntStringKeyPair tKeyValue( 5, std::string("Root of life") );
+  pTree->Insert(tKeyValue );
+  std::cerr << "insert 5 ok " << endl;
+  tKeyValue.SetKey(4);
+  pTree->Insert(tKeyValue );
+  std::cerr << "insert 4 ok " << endl;
+  tKeyValue.SetKey(3);
+  pTree->Insert(tKeyValue );
+  std::cerr << "insert 3 ok " << endl;
+  tKeyValue.SetKey(2);
+  pTree->Insert(tKeyValue );
+  std::cerr << "insert 2 ok " << endl;
+  tKeyValue.SetKey(1);
+  pTree->Insert(tKeyValue );
+  std::cerr << "insert 1 ok " << endl;
+  Handler handler;
+  pTree->ProcessInOrder<Handler>( handler, pTree->GetRoot() );
+  
+
+}
+/////////////////////////////////////////////////////////////////
+TEST( AVLTreeInsertLinear )
+{
   IntStringKeyPair tKeyValue( 0, std::string("Root of life") );
   CAVLTree<IntStringKeyPair > *pTree = new  CAVLTree<IntStringKeyPair >(); 
   std::cerr << "inserting, please wait: " << std::endl;
@@ -50,6 +79,52 @@ TEST( AVLTree )
   pTree->ProcessInOrder<Handler>( handler, pTree->GetRoot() );
   CHECK( 1.44f*(logf(MAX_TEST_NODES)/logf(2)) >= pTree->GetRoot()->GetHeight()  );
   std::cerr << "tree height: " << handler.maxheight << std::endl;
+  
+}
+/////////////////////////////////////////////////////////////////
+TEST( AVLTree_DoubleRotateLeft )
+{
+
+  CAVLTree<IntStringKeyPair > *pTree = new  CAVLTree<IntStringKeyPair >(); 
+
+  IntStringKeyPair tKeyValue( 5, std::string("Child of life") );  
+  pTree->Insert(tKeyValue );
+  tKeyValue.SetKey(2);  
+  pTree->Insert(tKeyValue );
+  tKeyValue.SetKey(8);  
+  pTree->Insert(tKeyValue );
+  tKeyValue.SetKey(6);  
+  pTree->Insert(tKeyValue );
+  tKeyValue.SetKey(9);  
+  pTree->Insert(tKeyValue );
+  tKeyValue.SetKey(7);  
+  pTree->Insert(tKeyValue );
+
+  Handler handler;
+  pTree->ProcessInOrder<Handler>( handler, pTree->GetRoot() );
+  
 
 }
 /////////////////////////////////////////////////////////////////
+TEST( AVLTree_DoubleRotateRight )
+{
+  
+  CAVLTree<IntStringKeyPair > *pTree = new  CAVLTree<IntStringKeyPair >(); 
+  IntStringKeyPair tKeyValue( 5, std::string("Child of life") );  
+  pTree->Insert(tKeyValue );
+  tKeyValue.SetKey(8);  
+  pTree->Insert(tKeyValue );
+  tKeyValue.SetKey(2);  
+  pTree->Insert(tKeyValue );
+  tKeyValue.SetKey(4);  
+  pTree->Insert(tKeyValue );
+  tKeyValue.SetKey(1);  
+  pTree->Insert(tKeyValue );
+  tKeyValue.SetKey(3);  
+  pTree->Insert(tKeyValue );
+  
+  Handler handler;
+  pTree->ProcessInOrder<Handler>( handler, pTree->GetRoot() );
+  
+
+}
