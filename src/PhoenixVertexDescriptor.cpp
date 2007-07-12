95dfe830421bf20bc2c8b36a4212debac5466530
entity <entity@cc4fdaca-9534-0410-bcb6-82ce2780185d>
2007-07-12 13:14:23 +0000

    first version
    git-svn-id: svn://localhost/phoenix/trunk@115 cc4fdaca-9534-0410-bcb6-82ce2780185d

---
+++ src/PhoenixVertexDescriptor.cpp
@@ -0,0 +1,40 @@
+#include "PhoenixVertexDescriptor.h"
+/////////////////////////////////////////////////////////////////
+Phoenix::Graphics::CVertexDescriptor::CVertexDescriptor( ELEMENT_TYPE nType, 
+							 unsigned int nNumElements)
+{
+  m_nType = nType;
+  switch ( GetType() )
+  {
+  case ELEMENT_TYPE_VERTEX_3F:
+    m_pData = new float[3*nNumElements];
+    break;
+  case ELEMENT_TYPE_TEX0_2F:
+    m_pData = new float[2*nNumElements];
+    break;
+  case ELEMENT_TYPE_COLOR_4UB:
+    m_pData = new unsigned char[4*nNumElements];
+    break;
+  case ELEMENT_TYPE_NULL:
+    m_pData = NULL;
+    break;
+  }
+}
+/////////////////////////////////////////////////////////////////
+Phoenix::Graphics::CVertexDescriptor::~CVertexDescriptor()
+{
+  switch ( GetType() )
+  {
+  case ELEMENT_TYPE_VERTEX_3F:
+  case ELEMENT_TYPE_TEX0_2F:
+    delete reinterpret_cast<float *>(m_pData);
+    break;
+  case ELEMENT_TYPE_COLOR_4UB:
+    delete reinterpret_cast<unsigned char *>(m_pData);
+    break;
+  case ELEMENT_TYPE_NULL:
+    m_pData = NULL;
+    break;
+  }
+}
+/////////////////////////////////////////////////////////////////
