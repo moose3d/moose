f913cade382acef7134ac216cb9c85c7284a3710
entity <entity@cc4fdaca-9534-0410-bcb6-82ce2780185d>
2007-07-13 20:59:17 +0000

    first version
    git-svn-id: svn://localhost/phoenix/trunk@126 cc4fdaca-9534-0410-bcb6-82ce2780185d

---
+++ tests/PhoenixResourceManagerTest.cpp
@@ -0,0 +1,41 @@
+#include <UnitTest++/UnitTest++.h>
+#include "PhoenixCore.h"
+#include "PhoenixResourceManager.h"
+#include <string>
+#include <iostream>
+/////////////////////////////////////////////////////////////////
+using namespace Phoenix::Core;
+/////////////////////////////////////////////////////////////////
+typedef CResourceManager<int, CHandle<int>, 7 > CIntResourceMgr;
+/////////////////////////////////////////////////////////////////
+TEST( PhoenixResourceManager )
+{
+
+  CIntResourceMgr *pMgr = CIntResourceMgr::GetInstance();
+  CIntResourceMgr *pMgr2 = CIntResourceMgr::GetInstance();
+  CHECK_EQUAL( pMgr, pMgr2 );
+
+}
+/////////////////////////////////////////////////////////////////
+TEST( PhoenixResourceManager_Create )
+{
+
+  CIntResourceMgr *pMgr = CIntResourceMgr::GetInstance();
+  CHandle<int> handle;
+  int iResource = 4;
+  CHandle<int> handleNotFound;
+  CHandle<int> handleNotFound2;
+  handleNotFound2.Initialize(65);
+  ////////////////////
+  std::string strResName("resourcetest");
+  std::string strSearch("resourcetest");
+  std::string strSearchNotFound("thisIsNotHere");
+  ////////////////////
+  CHECK_EQUAL( 0, pMgr->Create( &iResource, strResName, &handle ));
+  CHECK( pMgr->GetResource( handle ) == &iResource );
+  CHECK( pMgr->GetResource( strSearch) ==  &iResource );
+  CHECK( pMgr->GetResource( strSearchNotFound) ==  NULL );
+  CHECK( pMgr->GetResource( handleNotFound ) == NULL );
+  CHECK( pMgr->GetResource( handleNotFound2 ) == NULL );
+}
+/////////////////////////////////////////////////////////////////
