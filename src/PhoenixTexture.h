7ae1f8e34db7f87c1db96363f7a14d7e16cd1c53
entity <entity@cc4fdaca-9534-0410-bcb6-82ce2780185d>
2007-07-16 10:02:43 +0000

    first version
    git-svn-id: svn://localhost/phoenix/trunk@156 cc4fdaca-9534-0410-bcb6-82ce2780185d

---
+++ src/PhoenixTexture.h
@@ -0,0 +1,25 @@
+/////////////////////////////////////////////////////////////////
+namespace Phoenix
+{
+  namespace Graphics
+  {
+    enum TEXTURE_TYPE 
+    {
+      TEXTURE_2D = 0
+    };
+    ////////////////////    
+    /// Class for textures.
+    class COglTexture
+    {
+    private:
+      unsigned int m_nOglId;
+      TEXTURE_TYPE m_tTextureType;
+    public:
+      COglTexture( unsigned int nId, TEXTURE_TYPE tType );
+      ~COglTexture();
+      unsigned int GetID() const;
+      TEXTURE_TYPE GetType() const;
+    };
+  }; // namespace Graphics
+}; // namespace Phoenix
+/////////////////////////////////////////////////////////////////
