b77779b316882a5855fab54a82bf9ecc48594d10
entity <entity@cc4fdaca-9534-0410-bcb6-82ce2780185d>
2007-07-28 21:57:21 +0000

    first version
    git-svn-id: svn://localhost/phoenix/trunk@212 cc4fdaca-9534-0410-bcb6-82ce2780185d

---
+++ src/PhoenixShader.cpp
@@ -0,0 +1,42 @@
+#include "PhoenixShader.h"
+#include <GL/GLee.h>
+#include <GL/gl.h>
+/////////////////////////////////////////////////////////////////
+Phoenix::Graphics::CShader::CShader( unsigned int nProgramId ) : m_nShaderProgram(nProgramId), m_nVertexShader(0), m_nFragmentShader(0), m_bIsFragShader(0),m_bIsVertexShader(0)
+{
+  
+}
+/////////////////////////////////////////////////////////////////
+Phoenix::Graphics::CShader::~CShader()
+{
+  // detach shaders from program.
+  if ( m_bIsVertexShader )
+  {
+    glDetachShader( GetProgram(), GetVertexShader() );
+    glDeleteShader( GetVertexShader() );
+  }
+  if ( m_bIsFragShader ) 
+  {
+    glDetachShader( GetProgram(), GetFragmentShader() );
+    glDeleteShader( GetFragmentShader() );
+  }
+  // delete program
+  glDeleteProgram( GetProgram() );
+}
+/////////////////////////////////////////////////////////////////
+void 
+Phoenix::Graphics::CShader::SetVertexShader(unsigned int nVertexShader ) 
+{
+  m_nVertexShader = nVertexShader;
+  m_bIsVertexShader = 1;
+  glAttachShader( GetProgram(), GetVertexShader());
+}
+/////////////////////////////////////////////////////////////////
+void
+Phoenix::Graphics::CShader::SetFragmentShader(unsigned int nFragmentShader ) 
+{
+  m_nFragmentShader = nFragmentShader;
+  m_bIsFragShader = 1;
+  glAttachShader( GetProgram(), GetFragmentShader());
+}
+/////////////////////////////////////////////////////////////////
