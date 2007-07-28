7f92a42ea6bb7904f6ee31936e7d4b164b7611bf
entity <entity@cc4fdaca-9534-0410-bcb6-82ce2780185d>
2007-07-28 21:56:58 +0000

    shader handle, manager added
    git-svn-id: svn://localhost/phoenix/trunk@210 cc4fdaca-9534-0410-bcb6-82ce2780185d

--- src/PhoenixDefaultEntities.h
+++ src/PhoenixDefaultEntities.h
@@ -3,6 +3,7 @@
 /////////////////////////////////////////////////////////////////
 #include "PhoenixResourceManager.h"
 #include "PhoenixTexture.h"
+#include "PhoenixShader.h"
 /////////////////////////////////////////////////////////////////
 namespace Phoenix
 {
@@ -12,11 +13,13 @@ namespace Phoenix
     typedef Phoenix::Core::CHandle<Phoenix::Graphics::COglTexture>       TEXTURE_HANDLE;
     typedef Phoenix::Core::CHandle<Phoenix::Graphics::CVertexDescriptor> VERTEX_HANDLE;
     typedef Phoenix::Core::CHandle<Phoenix::Graphics::CIndexArray>       INDEX_HANDLE;
+    typedef Phoenix::Core::CHandle<Phoenix::Graphics::CShader>           SHADER_HANDLE;
     ////////////////////
     // Typedefs for default resource manager types.
     typedef Phoenix::Core::CResourceManager< Phoenix::Graphics::COglTexture,		TEXTURE_HANDLE>  TextureManager;
     typedef Phoenix::Core::CResourceManager< Phoenix::Graphics::CVertexDescriptor,	VERTEX_HANDLE>   VertexManager;
     typedef Phoenix::Core::CResourceManager< Phoenix::Graphics::CIndexArray,		INDEX_HANDLE>    IndexManager;
+    typedef Phoenix::Core::CResourceManager< Phoenix::Graphics::CShader,		SHADER_HANDLE>   ShaderManager;
     ////////////////////
 
   }; // namespace Default
@@ -26,6 +29,7 @@ namespace Phoenix
 #define g_DefaultTextureManager (Phoenix::Default::TextureManager::GetInstance())
 #define g_DefaultVertexManager  (Phoenix::Default::VertexManager::GetInstance())
 #define g_DefaultIndexManager   (Phoenix::Default::IndexManager::GetInstance())
+#define g_DefaultShaderManager   (Phoenix::Default::ShaderManager::GetInstance())
 ////////////////////
 // Constants
 #define TEXTURE_HANDLE_COUNT 8
