699703eecbbdade96122e8faceaae525785cc2ab
entity <entity@cc4fdaca-9534-0410-bcb6-82ce2780185d>
2007-07-29 11:58:01 +0000

    multitexturing fragment shader
    git-svn-id: svn://localhost/phoenix/trunk@229 cc4fdaca-9534-0410-bcb6-82ce2780185d

--- tests/Resources/Shaders/fragment.glsl
+++ tests/Resources/Shaders/fragment.glsl
@@ -1,5 +1,7 @@
+uniform sampler2D tex0,tex1;
 void main()
 {
-	/* green color */
-	gl_FragColor = vec4(0.0,1.0,0.0,1.0);	
+	vec4 color = texture2D(tex0,gl_TexCoord[0].st);
+	vec4 color2 = texture2D(tex1, gl_TexCoord[1].st);
+	gl_FragColor = color * color2;
 }
