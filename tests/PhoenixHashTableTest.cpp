ad54be2557eab797bfc2e961f51d6f4f11d4abef
entity <entity@cc4fdaca-9534-0410-bcb6-82ce2780185d>
2007-08-07 08:47:11 +0000

    delete test added
    git-svn-id: svn://localhost/phoenix/trunk@294 cc4fdaca-9534-0410-bcb6-82ce2780185d

--- tests/PhoenixHashTableTest.cpp
+++ tests/PhoenixHashTableTest.cpp
@@ -12,7 +12,7 @@ TEST( CHashTable )
   unsigned int nObject = 12;
   tItem.SetObject( nObject );
   tItem.SetKey( key );
-
+  // Insert items "oneshot" and "twelve"
   pTable->Insert( tItem );
   CHashItem<std::string,unsigned int> *pItem =  pTable->Find( key );
   CHECK( pItem != NULL );
@@ -22,7 +22,8 @@ TEST( CHashTable )
   tItem.SetKey( (key = "oneshot"));
   tItem.SetObject((nObject = 1)) ; 
   pTable->Insert(tItem);
-
+  ////////////////////
+  // Find those keys, plus one which should not be there.
   pItem =  pTable->Find(   (key = "twelve") );
   CHECK( pItem != NULL );
   if( pItem != NULL )
@@ -32,9 +33,30 @@ TEST( CHashTable )
   CHECK( pItem != NULL );
   if( pItem != NULL )
     CHECK( pItem->GetObject() == 1 );
-
+  
   pItem =  pTable->Find(   (key="nothere") );
   CHECK( pItem == NULL );
+  ////////////////////
+  // Delete key "twelve", should leave "oneshot"
+  pTable->Delete( (key="twelve"));
+
+  pItem =  pTable->Find(   (key = "twelve") );
+  CHECK( pItem == NULL );
+
+  pItem =  pTable->Find(   (key="oneshot") );
+  CHECK( pItem != NULL );
+  if( pItem != NULL )
+    CHECK( pItem->GetObject() == 1 );
+  ////////////////////
+  // Delete "oneshot", should leave nothing
+  pTable->Delete( (key="oneshot"));
+  pItem =  pTable->Find(   (key="oneshot") );
+  CHECK( pItem == NULL );
+  pItem =  pTable->Find(   (key = "twelve") );
+  CHECK( pItem == NULL );
+  pItem =  pTable->Find(   (key="nothere") );
+  CHECK( pItem == NULL );
+
 }
 
 
