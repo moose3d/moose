32e337c07fd561440efd9eeac30f531e17f18c51
entity <entity@cc4fdaca-9534-0410-bcb6-82ce2780185d>
2007-08-07 11:05:33 +0000

    delete test added
    git-svn-id: svn://localhost/phoenix/trunk@297 cc4fdaca-9534-0410-bcb6-82ce2780185d

--- tests/PhoenixResourceManagerTest.cpp
+++ tests/PhoenixResourceManagerTest.cpp
@@ -32,7 +32,8 @@ TEST( PhoenixResourceManager_Create )
 
   CIntResourceMgr *pMgr = CIntResourceMgr::GetInstance();
   CHandle<int> handle;
-  int iResource = 4;
+  int *pResource = new int;
+  *pResource = 4;
   CHandle<int> handleNotFound;
   CHandle<int> handleNotFound2;
   handleNotFound2.Initialize(65);
@@ -41,11 +42,80 @@ TEST( PhoenixResourceManager_Create )
   std::string strSearch("resourcetest");
   std::string strSearchNotFound("thisIsNotHere");
   ////////////////////
-  CHECK_EQUAL( 0, pMgr->Create( &iResource, strResName, handle ));
-  CHECK( pMgr->GetResource( handle ) == &iResource );
-  CHECK( pMgr->GetResource( strSearch) ==  &iResource );
+  CHECK_EQUAL( 0, pMgr->Create( pResource, strResName, handle ));
+  CHECK( pMgr->GetResource( handle ) == pResource );
+  CHECK( pMgr->GetResource( strSearch) ==  pResource );
   CHECK( pMgr->GetResource( strSearchNotFound) ==  NULL );
   CHECK( pMgr->GetResource( handleNotFound ) == NULL );
   CHECK( pMgr->GetResource( handleNotFound2 ) == NULL );
+  
+  CIntResourceMgr::DestroyInstance();
+  
+}
+/////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////
+TEST( PhoenixResourceManager_Delete )
+{
+
+  CIntResourceMgr *pMgr = CIntResourceMgr::GetInstance();
+  CHandle<int> handle1;
+  CHandle<int> handle2;
+  CHandle<int> handle3;
+  CHandle<int> handle4;
+  CHandle<int> handle5;
+  CHandle<int> handleNull;
+  
+  handle5.Initialize(5);
+  
+  int *pIntRes1 = new int;
+  int *pIntRes2 = new int;
+  int *pIntRes3 = new int;
+  int *pIntRes4 = new int;
+
+  *pIntRes1 = 1;
+  *pIntRes2 = 2;
+  *pIntRes3 = 3;
+  *pIntRes4 = 4;
+  
+  ////////////////////
+  std::string strResName;
+  ////////////////////
+  /// Create resources
+  CHECK_EQUAL( 0, pMgr->Create( pIntRes1, (strResName = "one"), handle1 ));
+  CHECK_EQUAL( 0, pMgr->Create( pIntRes2, (strResName = "two"), handle2 ));
+  CHECK_EQUAL( 0, pMgr->Create( pIntRes3, (strResName = "three"), handle3 ));
+  CHECK_EQUAL( 0, pMgr->Create( pIntRes4, (strResName = "four"), handle4 ));
+
+  /// Check that resources exist
+  CHECK( pMgr->GetResource( handle1 ) == pIntRes1 );
+  CHECK( pMgr->GetResource( handle2 ) == pIntRes2 );
+  CHECK( pMgr->GetResource( handle3 ) == pIntRes3 );
+  CHECK( pMgr->GetResource( handle4 ) == pIntRes4 );
+  CHECK( pMgr->GetResource( handle5 ) == NULL );
+  CHECK( pMgr->GetResource( handleNull ) == NULL );
+
+  CHECK_EQUAL( 0, handle1.GetIndex());
+  CHECK_EQUAL( 1, handle2.GetIndex());
+  CHECK_EQUAL( 2, handle3.GetIndex());
+  CHECK_EQUAL( 3, handle4.GetIndex());
+
+  // Verify that they something else is not found.
+  CHECK( pMgr->GetResource( (strResName = "five")) ==  NULL );
+  CHECK( pMgr->GetResource( (strResName = "zero")) ==  NULL );
+  CHECK( pMgr->GetResource( (strResName = "")) ==  NULL );
+
+  pMgr->Destroy( (strResName="one") );
+
+  CHECK( pMgr->GetResource( handle1 ) == NULL );
+  CHECK( pMgr->GetResource( handle2 ) == pIntRes2 );
+  CHECK( pMgr->GetResource( handle3 ) == pIntRes3 );
+  CHECK( pMgr->GetResource( handle4 ) == pIntRes4 );
+  CHECK( pMgr->GetResource( handle5 ) == NULL );
+  CHECK( pMgr->GetResource( handleNull ) == NULL );
+  CHECK( handle1.IsNull());
+  CHECK_EQUAL( 1, handle2.GetIndex());
+  CHECK_EQUAL( 2, handle3.GetIndex());
+  CHECK_EQUAL( 0, handle4.GetIndex());
+  
 }
 /////////////////////////////////////////////////////////////////
