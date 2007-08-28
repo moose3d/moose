900220962ff94d5d6b14805947feadb7f5ca1e7d
entity <entity@cc4fdaca-9534-0410-bcb6-82ce2780185d>
2007-08-28 10:27:12 +0000

    shader fixes
    git-svn-id: svn://localhost/phoenix/trunk@491 cc4fdaca-9534-0410-bcb6-82ce2780185d

--- tests/Resources/Shaders/bump_frag.glsl
+++ tests/Resources/Shaders/bump_frag.glsl
@@ -14,7 +14,7 @@ void main (void)
 	vec3 vVec = normalize(eyeVec);
 	
 	vec4 base = vec4(texture2D(colorMap, texCoord).xyz,1);
-	
+
 	vec3 bump = normalize( texture2D(normalMap, texCoord).xyz * 2.0 - 1.0);
 
 	vec4 vAmbient = gl_LightSource[0].ambient * gl_FrontMaterial.ambient;
@@ -27,9 +27,13 @@ void main (void)
 	float specular = pow(clamp(dot(reflect(-lVec, bump), vVec), 0.0, 1.0), 
 	                 gl_FrontMaterial.shininess );
 	
-	vec4 vSpecular = gl_LightSource[0].specular * gl_FrontMaterial.specular * 
-					 specular;	
-	
+	vec4 vSpecular;
+
+	if ( texture2D(colorMap, texCoord).a == 0.0 ){
+		vSpecular = vec4(0,0,0,0); 
+	} else {
+		vSpecular = gl_LightSource[0].specular * gl_FrontMaterial.specular * specular;	
+	}
 	gl_FragColor = ( vAmbient*base + vDiffuse*base + vSpecular) * att;
 
 }
