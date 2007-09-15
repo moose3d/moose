5fa899482c5f2a75edee1629f72a87cbe5f86656
entity <entity@cc4fdaca-9534-0410-bcb6-82ce2780185d>
2007-09-15 13:36:59 +0000

    working volumeline
    git-svn-id: svn://localhost/phoenix/trunk@530 cc4fdaca-9534-0410-bcb6-82ce2780185d

--- tests/Resources/Shaders/line_vertex.glsl
+++ tests/Resources/Shaders/line_vertex.glsl
@@ -4,23 +4,23 @@ vector3 v1,v2,v3,v4 <- We're drawing a quad, so they are calculated, but must be
 float thickness  <- given for each vertex, either -v or +v.
 */
 
-attribute vec4 linedirThickness;
+attribute vec4 endposThickness;
 uniform vec3 cameraPos;
+varying vec4 color;
 
 void main()
 {
-	vec3 linedir = linedirThickness.xyz;
-	vec3 eyeDir = cameraPos - gl_Vertex.xyz;
+	vec3 endPos = endposThickness.xyz;
+	vec3 linedir = endPos - gl_Vertex.xyz;				
+
 	vec3 offsetVec = normalize(cross(cameraPos,linedir));
-	vec3 middlePoint = gl_Vertex.xyz + (linedir * 0.5);
-	float texcoef = dot( normalize(linedir),middlePoint );
-	vec3 posOffset = gl_Vertex.xyz + (offsetVec * linedirThickness.w);
-	vec3 perpVector = normalize(cross(offsetVec,linedir));
-	posOffset = (perpVector * (texcoef * abs(linedirThickness.w))) + posOffset;
+ 	vec3 normal = normalize(cross(linedir, offsetVec));
+	vec3 eyeToStart = gl_Vertex.xyz - cameraPos;
+
+	/*float texcoef = abs( dot( normalize(linedir), normalize(eyeToStart) ));*/
+	vec3 posOffset = gl_Vertex.xyz + (offsetVec * endposThickness.w);
 	gl_Position = gl_ModelViewProjectionMatrix * vec4(posOffset.xyz,gl_Vertex.w);	
-
-
-
+	gl_TexCoord[0] = gl_MultiTexCoord0;
 }
 
 
