55d88e035a51fb6480782c064c100a132a5df8a5
entity <entity@cc4fdaca-9534-0410-bcb6-82ce2780185d>
2007-09-23 09:15:47 +0000

    left old code might might be useful
    git-svn-id: svn://localhost/phoenix/trunk@554 cc4fdaca-9534-0410-bcb6-82ce2780185d

--- src/PhoenixOctree.cpp
+++ src/PhoenixOctree.cpp
@@ -1,47 +1,24 @@
-/******************************************************************
- *   Copyright(c) 2006,2007 eNtity/Anssi Gröhn
- * 
- *   This file is part of GSE.
- *
- *   GSE is free software; you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation; either version 2 of the License, or
- *   (at your option) any later version.
- *
- *    GSE is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU General Public License for more details.
- *
- *   You should have received a copy of the GNU General Public License
- *   along with GSE; if not, write to the Free Software
- *   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
- *
- ******************************************************************/
-/////////////////////////////////////////////////////////////////
-#include "GSE_Octree.h"
-#include "GSE_Logger.h"
-#include "GSE_structures.h"
-#include "GSE_GraphNodes.h"
+#include "PhoenixOctree.h"
+
 #include <iostream>
 /////////////////////////////////////////////////////////////////
 using std::cerr;
 using std::endl;
 /////////////////////////////////////////////////////////////////
-int IsInside(  GSE_Vector3 &vCenter,
+int IsInside(  CVector3 &vCenter,
 	       float fHalfWidth, float fHalfHeight, float fHalfLength,
-	       GSE_Vector3 &vPosition)
+	       CVector3 &vPosition)
 {
   int bRetval = 0;
 
 #define EPSILON 0.0001f
-  if ( vPosition[GSE_Vector3::X] <= vCenter[GSE_Vector3::X]+(fHalfWidth)+EPSILON &&
-       vPosition[GSE_Vector3::X] >= vCenter[GSE_Vector3::X]-(fHalfWidth)-EPSILON){
+  if ( vPosition[CVector3::X] <= vCenter[CVector3::X]+(fHalfWidth)+EPSILON &&
+       vPosition[CVector3::X] >= vCenter[CVector3::X]-(fHalfWidth)-EPSILON){
     
-    if ( vPosition[GSE_Vector3::Y] <= vCenter[GSE_Vector3::Y]+(fHalfHeight)+EPSILON &&
-	 vPosition[GSE_Vector3::Y] >=  vCenter[GSE_Vector3::Y]-(fHalfHeight)-EPSILON){
-      if ( vPosition[GSE_Vector3::Z] <= vCenter[GSE_Vector3::Z]+(fHalfLength)+EPSILON &&
-	   vPosition[GSE_Vector3::Z] >= vCenter[GSE_Vector3::Z]-(fHalfLength)-EPSILON){
+    if ( vPosition[CVector3::Y] <= vCenter[CVector3::Y]+(fHalfHeight)+EPSILON &&
+	 vPosition[CVector3::Y] >=  vCenter[CVector3::Y]-(fHalfHeight)-EPSILON){
+      if ( vPosition[CVector3::Z] <= vCenter[CVector3::Z]+(fHalfLength)+EPSILON &&
+	   vPosition[CVector3::Z] >= vCenter[CVector3::Z]-(fHalfLength)-EPSILON){
 	bRetval = 1;
       } 
     } 
@@ -49,87 +26,23 @@ int IsInside(  GSE_Vector3 &vCenter,
   
   return bRetval;
 }
-/////////////////////////////////////////////////////////////////
-/// Macro for easier deletion
-#define DELETE(A) \
- {\
-  if ( A != NULL ){\
-    delete A;\
-    A = NULL;\
-  }\
-}
-/////////////////////////////////////////////////////////////////
-GSE_Octree::GSE_Octree()
-{
-  // initialize nodes 
-  m_pNodes[TOP_LEFT_FRONT] = NULL;
-  m_pNodes[TOP_LEFT_BACK] = NULL;
-  m_pNodes[TOP_RIGHT_FRONT] = NULL;
-  m_pNodes[TOP_RIGHT_BACK] = NULL;
-  m_pNodes[BOTTOM_LEFT_FRONT] = NULL;
-  m_pNodes[BOTTOM_LEFT_BACK] = NULL;
-  m_pNodes[BOTTOM_RIGHT_FRONT] = NULL;
-  m_pNodes[BOTTOM_RIGHT_BACK] = NULL;
-}
-/////////////////////////////////////////////////////////////////
-void
-GSE_Octree::Init()
-{
 
-  DELETE(m_pNodes[TOP_LEFT_FRONT]);
-  DELETE(m_pNodes[TOP_LEFT_BACK]);
-  DELETE(m_pNodes[TOP_RIGHT_FRONT]);
-  DELETE(m_pNodes[TOP_RIGHT_BACK]);
-  DELETE(m_pNodes[BOTTOM_LEFT_FRONT]);
-  DELETE(m_pNodes[BOTTOM_LEFT_BACK]);
-  DELETE(m_pNodes[BOTTOM_RIGHT_FRONT]);
-  DELETE(m_pNodes[BOTTOM_RIGHT_BACK]);
-  
-}
-/////////////////////////////////////////////////////////////////
-GSE_Octree::~GSE_Octree()
-{
-  Init();
-}
-/////////////////////////////////////////////////////////////////
-GSE_Octree *
-GSE_Octree::operator[]( Octree_Section iSection )
-{
-  return m_pNodes[iSection];
-}
-/////////////////////////////////////////////////////////////////
-void
-GSE_Octree::SetNode( Octree_Section iSection, GSE_Octree *pNode)
-{
-  m_pNodes[iSection] = pNode;
-}
-/////////////////////////////////////////////////////////////////
-void
-GSE_Octree::DeleteNode( Octree_Section iSection )
-{
-  delete m_pNodes[iSection];
-  m_pNodes[iSection] = NULL;
-}
-/////////////////////////////////////////////////////////////////
-GSE_Octree **
-GSE_Octree::GetNodes(){
 
-  return m_pNodes;
-} 
 /////////////////////////////////////////////////////////////////
-GSE_GeometryOctree *
-GSE_GeometryOctree::Split( VertexDescriptor pVertices, 
+
+CGeometryOctree *
+CGeometryOctree::Split( VertexDescriptor pVertices, 
 			   std::list<unsigned int> & lstIndices,
 			   unsigned int nMaxTreeDepth, 
 			   unsigned int nMinFacesInNode,
 			   float fWidth,
 			   float fHeight,
 			   float fLength,
-			   const GSE_Vector3 &vCenter,
-			   GSE_RenderGraph &rRenderGraph)
+			   const CVector3 &vCenter,
+			   CRenderGraph &rRenderGraph)
 {
   
-  GSE_GeometryOctree *pOctree  = NULL;
+  CGeometryOctree *pOctree  = NULL;
   
   std::list<unsigned int> lstNode1;
   std::list<unsigned int> lstNode2;
@@ -141,20 +54,20 @@ GSE_GeometryOctree::Split( VertexDescriptor pVertices,
   std::list<unsigned int> lstNode8;
   std::list<unsigned int>::iterator it;
   
-  GSE_Vector3 vTopLeftFront;
-  GSE_Vector3 vTopLeftBack;
-  GSE_Vector3 vTopRightFront;
-  GSE_Vector3 vTopRightBack;
+  CVector3 vTopLeftFront;
+  CVector3 vTopLeftBack;
+  CVector3 vTopRightFront;
+  CVector3 vTopRightBack;
   
-  GSE_Vector3 vBottomLeftFront;
-  GSE_Vector3 vBottomLeftBack;
-  GSE_Vector3 vBottomRightFront;
-  GSE_Vector3 vBottomRightBack;
+  CVector3 vBottomLeftFront;
+  CVector3 vBottomLeftBack;
+  CVector3 vBottomRightFront;
+  CVector3 vBottomRightBack;
   
-  GSE_Vector3 vVec1;
-  GSE_Vector3 vVec2;
-  GSE_Vector3 vVec3;
-  GSE_Vector3 vTmp;
+  CVector3 vVec1;
+  CVector3 vVec2;
+  CVector3 vVec3;
+  CVector3 vTmp;
   float fWidthDiv2  = fWidth * 0.5f;
   float fHeightDiv2 = fHeight * 0.5f;
   float fLengthDiv2 = fLength * 0.5f;
@@ -181,38 +94,38 @@ GSE_GeometryOctree::Split( VertexDescriptor pVertices,
       vVec2.UseExternalData(&(pVertices[index2*3]));
       vVec3.UseExternalData(&(pVertices[index3*3]));
 
-      vTmp[GSE_Vector3::X] = -fWidthDiv4;
-      vTmp[GSE_Vector3::Y] =  fHeightDiv4;
-      vTmp[GSE_Vector3::Z] =  fLengthDiv4;
+      vTmp[CVector3::X] = -fWidthDiv4;
+      vTmp[CVector3::Y] =  fHeightDiv4;
+      vTmp[CVector3::Z] =  fLengthDiv4;
       vTopLeftFront  = vCenter + vTmp;
-      vTmp[GSE_Vector3::X] = -fWidthDiv4;
-      vTmp[GSE_Vector3::Y] =  fHeightDiv4;
-      vTmp[GSE_Vector3::Z] = -fLengthDiv4;
+      vTmp[CVector3::X] = -fWidthDiv4;
+      vTmp[CVector3::Y] =  fHeightDiv4;
+      vTmp[CVector3::Z] = -fLengthDiv4;
       vTopLeftBack   = vCenter + vTmp;
-      vTmp[GSE_Vector3::X] =  fWidthDiv4;
-      vTmp[GSE_Vector3::Y] =  fHeightDiv4;
-      vTmp[GSE_Vector3::Z] =  fLengthDiv4;
+      vTmp[CVector3::X] =  fWidthDiv4;
+      vTmp[CVector3::Y] =  fHeightDiv4;
+      vTmp[CVector3::Z] =  fLengthDiv4;
       vTopRightFront = vCenter + vTmp;
-      vTmp[GSE_Vector3::X] =  fWidthDiv4;
-      vTmp[GSE_Vector3::Y] =  fHeightDiv4;
-      vTmp[GSE_Vector3::Z] =  -fLengthDiv4;
+      vTmp[CVector3::X] =  fWidthDiv4;
+      vTmp[CVector3::Y] =  fHeightDiv4;
+      vTmp[CVector3::Z] =  -fLengthDiv4;
       vTopRightBack  = vCenter + vTmp;
       /////////////////////////////////////////////////////////////////
-      vTmp[GSE_Vector3::X] =  -fWidthDiv4;
-      vTmp[GSE_Vector3::Y] =  -fHeightDiv4;
-      vTmp[GSE_Vector3::Z] =   fLengthDiv4;
+      vTmp[CVector3::X] =  -fWidthDiv4;
+      vTmp[CVector3::Y] =  -fHeightDiv4;
+      vTmp[CVector3::Z] =   fLengthDiv4;
       vBottomLeftFront  = vCenter + vTmp;
-      vTmp[GSE_Vector3::X] =  -fWidthDiv4;
-      vTmp[GSE_Vector3::Y] =  -fHeightDiv4;
-      vTmp[GSE_Vector3::Z] =  -fLengthDiv4;
+      vTmp[CVector3::X] =  -fWidthDiv4;
+      vTmp[CVector3::Y] =  -fHeightDiv4;
+      vTmp[CVector3::Z] =  -fLengthDiv4;
       vBottomLeftBack   = vCenter + vTmp;
-      vTmp[GSE_Vector3::X] =   fWidthDiv4;
-      vTmp[GSE_Vector3::Y] =  -fHeightDiv4;
-      vTmp[GSE_Vector3::Z] =   fLengthDiv4;
+      vTmp[CVector3::X] =   fWidthDiv4;
+      vTmp[CVector3::Y] =  -fHeightDiv4;
+      vTmp[CVector3::Z] =   fLengthDiv4;
       vBottomRightFront = vCenter + vTmp;
-      vTmp[GSE_Vector3::X] =   fWidthDiv4;
-      vTmp[GSE_Vector3::Y] =  -fHeightDiv4;
-      vTmp[GSE_Vector3::Z] =  -fLengthDiv4;
+      vTmp[CVector3::X] =   fWidthDiv4;
+      vTmp[CVector3::Y] =  -fHeightDiv4;
+      vTmp[CVector3::Z] =  -fLengthDiv4;
       vBottomRightBack  = vCenter + vTmp;
 
       // Sort the indices by faces 
@@ -296,64 +209,64 @@ GSE_GeometryOctree::Split( VertexDescriptor pVertices,
     lstIndices.clear();
 
     // Assign proper dimensions and position
-    pOctree = new GSE_GeometryOctree();
+    pOctree = new CGeometryOctree();
     pOctree->SetWidth(fWidth);
     pOctree->SetHeight(fHeight);
     pOctree->SetLength(fLength);
     pOctree->SetPosition(vCenter);
     //cerr << "Subdividing at level " << nMaxTreeDepth << endl;
     // Call split for each child node recursively.
-    pOctree->SetNode( GSE_Octree::TOP_LEFT_FRONT, 
-		      GSE_GeometryOctree::Split( pVertices, lstNode1,
+    pOctree->SetNode( COctree::TOP_LEFT_FRONT, 
+		      CGeometryOctree::Split( pVertices, lstNode1,
 						 nMaxTreeDepth-1,nMinFacesInNode,
 						 fWidthDiv2, fHeightDiv2,
 						 fLengthDiv2, vTopLeftFront,
 						 rRenderGraph));
     
-    pOctree->SetNode( GSE_Octree::TOP_LEFT_BACK, 
-		      GSE_GeometryOctree::Split( pVertices, lstNode2,
+    pOctree->SetNode( COctree::TOP_LEFT_BACK, 
+		      CGeometryOctree::Split( pVertices, lstNode2,
 						 nMaxTreeDepth-1,nMinFacesInNode,
 						 fWidthDiv2, fHeightDiv2,
 						 fLengthDiv2, vTopLeftBack,
 						 rRenderGraph));
     
-    pOctree->SetNode( GSE_Octree::TOP_RIGHT_FRONT, 
-		      GSE_GeometryOctree::Split( pVertices, lstNode3,
+    pOctree->SetNode( COctree::TOP_RIGHT_FRONT, 
+		      CGeometryOctree::Split( pVertices, lstNode3,
 						 nMaxTreeDepth-1,nMinFacesInNode,
 						 fWidthDiv2, fHeightDiv2,
 						 fLengthDiv2, vTopRightFront,
 						 rRenderGraph));
     
-    pOctree->SetNode( GSE_Octree::TOP_RIGHT_BACK, 
-		      GSE_GeometryOctree::Split( pVertices, lstNode4,
+    pOctree->SetNode( COctree::TOP_RIGHT_BACK, 
+		      CGeometryOctree::Split( pVertices, lstNode4,
 						 nMaxTreeDepth-1,nMinFacesInNode,
 						 fWidthDiv2, fHeightDiv2,
 						 fLengthDiv2, vTopRightBack,
 						 rRenderGraph));
     
-    pOctree->SetNode( GSE_Octree::BOTTOM_LEFT_FRONT, 
-		      GSE_GeometryOctree::Split( pVertices, lstNode5,
+    pOctree->SetNode( COctree::BOTTOM_LEFT_FRONT, 
+		      CGeometryOctree::Split( pVertices, lstNode5,
 						 nMaxTreeDepth-1,nMinFacesInNode,
 						 fWidthDiv2, fHeightDiv2,
 						 fLengthDiv2, vBottomLeftFront,
 						 rRenderGraph));
 
-    pOctree->SetNode( GSE_Octree::BOTTOM_LEFT_BACK, 
-		      GSE_GeometryOctree::Split( pVertices, lstNode6,
+    pOctree->SetNode( COctree::BOTTOM_LEFT_BACK, 
+		      CGeometryOctree::Split( pVertices, lstNode6,
 						 nMaxTreeDepth-1,nMinFacesInNode,
 						 fWidthDiv2, fHeightDiv2,
 						 fLengthDiv2, vBottomLeftBack,
 						 rRenderGraph));
     
-    pOctree->SetNode( GSE_Octree::BOTTOM_RIGHT_FRONT, 
-		      GSE_GeometryOctree::Split( pVertices, lstNode7,
+    pOctree->SetNode( COctree::BOTTOM_RIGHT_FRONT, 
+		      CGeometryOctree::Split( pVertices, lstNode7,
 						 nMaxTreeDepth-1,nMinFacesInNode,
 						 fWidthDiv2, fHeightDiv2,
 						 fLengthDiv2, vBottomRightFront,
 						 rRenderGraph));
     
-    pOctree->SetNode( GSE_Octree::BOTTOM_RIGHT_BACK, 
-		      GSE_GeometryOctree::Split( pVertices, lstNode8,
+    pOctree->SetNode( COctree::BOTTOM_RIGHT_BACK, 
+		      CGeometryOctree::Split( pVertices, lstNode8,
 						 nMaxTreeDepth-1,nMinFacesInNode,
 						 fWidthDiv2, fHeightDiv2,
 						 fLengthDiv2, vBottomRightBack,
@@ -371,7 +284,7 @@ GSE_GeometryOctree::Split( VertexDescriptor pVertices,
     else 
     {
 
-      pOctree = new GSE_GeometryOctree();
+      pOctree = new CGeometryOctree();
       // Assign proper dimensions and position for the box
       pOctree->SetWidth(fWidth);
       pOctree->SetHeight(fHeight);
@@ -379,8 +292,8 @@ GSE_GeometryOctree::Split( VertexDescriptor pVertices,
       pOctree->SetPosition(vCenter);
 
       // Generate indexbuffer
-      GSE_IndexBuffer *pIndexBuffer = GSE_IndexBufferMgr::GetInstance()->CreateIndexBuffer();
-      GSE_IndexBufferNode *pIndexBufferNode = rRenderGraph.CreateNode<GSE_IndexBufferNode>();
+      CIndexBuffer *pIndexBuffer = CIndexBufferMgr::GetInstance()->CreateIndexBuffer();
+      CIndexBufferNode *pIndexBufferNode = rRenderGraph.CreateNode<CIndexBufferNode>();
       pIndexBufferNode->SetIndexBuffer( pIndexBuffer );
       pOctree->SetIndexBufferNode( pIndexBufferNode );
       pOctree->GetIndexBufferNode()->GetIndexBuffer()->Init(lstIndices.size());
