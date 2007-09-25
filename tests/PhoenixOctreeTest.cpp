eaf850eb4389b27afdfff9357c4f4f42ff14efb2
entity <entity@cc4fdaca-9534-0410-bcb6-82ce2780185d>
2007-09-25 21:14:41 +0000

    octree test
    git-svn-id: svn://localhost/phoenix/trunk@561 cc4fdaca-9534-0410-bcb6-82ce2780185d

---
+++ tests/PhoenixOctreeTest.cpp
@@ -0,0 +1,17 @@
+#include <UnitTest++/UnitTest++.h>
+#include "PhoenixOctree.h"
+#include <string>
+#include <iostream>
+using namespace Phoenix::Spatial;
+using namespace std;
+/////////////////////////////////////////////////////////////////
+TEST( COctree )
+{
+  
+  COctree<float> *pOctree = new COctree<float>( 6, 100.0f);
+  
+
+  
+}
+
+
