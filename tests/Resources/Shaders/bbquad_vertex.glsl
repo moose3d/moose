d0b72c165cd73d980478cb2d0130337357780d6a
entity <entity@cc4fdaca-9534-0410-bcb6-82ce2780185d>
2007-11-01 16:08:18 +0000

    camera space calculations
    git-svn-id: svn://localhost/phoenix/trunk@673 cc4fdaca-9534-0410-bcb6-82ce2780185d

--- tests/Resources/Shaders/bbquad_vertex.glsl
+++ tests/Resources/Shaders/bbquad_vertex.glsl
@@ -1,21 +1,18 @@
-uniform vec3 camerapos;
+
 attribute vec4 dirWidth;
 varying float alpha;
 void main()
 {
 
-	vec3 start = gl_Vertex.xyz;
-	vec3 end   = gl_Vertex.xyz-dirWidth.xyz;
-	vec3 linedir = end-start;
-	vec3 eyeDir = start-camerapos;
-	vec3 off = normalize(cross(eyeDir,linedir));
-		
-	gl_TexCoord[0].x = gl_MultiTexCoord0.x;
-	gl_TexCoord[0].y = gl_MultiTexCoord0.y;
-	float coef = abs( dot( normalize(linedir), normalize(eyeDir) ));
-
+	vec4 start = gl_ModelViewMatrix * gl_Vertex;
+	vec4 end   = gl_ModelViewMatrix * vec4(gl_Vertex.xyz-dirWidth.xyz,gl_Vertex.w);
+	vec4 linedir = vec4(end.xyz-start.xyz,0);
+	vec3 off = normalize(cross(-start.xyz,linedir.xyz));
+	float coef = abs( dot( normalize(linedir.xyz), normalize(start.xyz) ));
 	alpha = smoothstep(0.15, 0.35, coef);
 
-	gl_Position = gl_ModelViewProjectionMatrix * vec4(start.xyz+(off*dirWidth.w*0.79),gl_Vertex.w);
+	gl_Position = gl_ProjectionMatrix * vec4(start.xyz+(off*dirWidth.w*0.79),start.w);
+	gl_TexCoord[0].x = gl_MultiTexCoord0.x;
+	gl_TexCoord[0].y = gl_MultiTexCoord0.y;
 }
 
