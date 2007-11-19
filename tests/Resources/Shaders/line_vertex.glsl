6c7fce8b75ecd88f812e1460596822efe3aa7404
eNtity <entity@iki.fi>
2007-11-19 20:00:41 +0200

    fixed coordinate bug

--- tests/Resources/Shaders/line_vertex.glsl
+++ tests/Resources/Shaders/line_vertex.glsl
@@ -4,8 +4,8 @@ uniform float time;
 void main()
 {
 	/* into EYE coordinates */	
-	vec4 endPos = gl_ModelViewProjectionMatrix * vec4(endposThickness.xyz,gl_Vertex.w);
-	vec4 startPos = gl_ModelViewProjectionMatrix * gl_Vertex;
+	vec4 endPos = gl_ModelViewMatrix * vec4(endposThickness.xyz,gl_Vertex.w);
+	vec4 startPos = gl_ModelViewMatrix * gl_Vertex;
 
 	vec3 linedir = (endPos - startPos).xyz;
 	vec3 offsetVec = normalize(cross(startPos.xyz,linedir));
@@ -14,7 +14,8 @@ void main()
 	/*float texcoef = abs( dot( normalize(linedir), normalize(eyeToStart) ));*/
 
 	startPos = vec4(startPos.xyz + (offsetVec * endposThickness.w),startPos.w);
-	gl_Position = startPos;
+	/* into window coordinates */
+	gl_Position = gl_ProjectionMatrix * startPos;
 	gl_TexCoord[0].x = (gl_MultiTexCoord0.x * length(endPos - startPos))+ time;
 	gl_TexCoord[0].y = gl_MultiTexCoord0.y;
 }
