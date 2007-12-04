e177f93380f1b9dc3157773e65687877f6fa25b6
entity <entity@iki.fi>
2007-12-04 22:54:58 +0200

    uses alpha

--- tests/Resources/Shaders/line_frag.glsl
+++ tests/Resources/Shaders/line_frag.glsl
@@ -1,5 +1,7 @@
 uniform sampler2D beamTexture;
+varying float alpha;
 void main()
 {
-	gl_FragColor = texture2D(beamTexture,gl_TexCoord[0].st);
+	gl_FragColor = vec4(texture2D(beamTexture,gl_TexCoord[0].st).xyz,alpha);
+
 }
