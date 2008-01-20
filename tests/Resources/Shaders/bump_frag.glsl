fd432c759f7784b8ac1b85a7b184bfb1312b345b
eNtity <entity@iki.fi>
2008-01-20 21:47:34 +0200

    bump mapping for directional light

--- tests/Resources/Shaders/bump_frag.glsl
+++ tests/Resources/Shaders/bump_frag.glsl
@@ -3,27 +3,23 @@ varying vec3 eyeVec;
 varying vec2 texCoord;
 uniform sampler2D colorMap;
 uniform sampler2D normalMap;
-uniform float invRadius;
 
 void main (void)
 {
-	float distSqr = dot(lightVec, lightVec);
-	float att = clamp(1.0 - invRadius * sqrt(distSqr), 0.0, 1.0);
-	att = 1.0; 
-	vec3 lVec = lightVec * inversesqrt(distSqr);
-
+	float att = 1.0; 
+	vec3 lVec = lightVec;
 	vec3 vVec = normalize(eyeVec);
-	
 	vec4 base = vec4(texture2D(colorMap, texCoord).xyz,1);
-
-	vec3 bump = normalize( texture2D(normalMap, texCoord).xyz * 2.0 - 1.0);
-
-	vec4 vAmbient = gl_LightSource[0].ambient * gl_FrontMaterial.ambient + gl_LightModel.ambient * gl_FrontMaterial.ambient;
+	vec3 bump = normalize( texture2D(normalMap, 
+				         texCoord).xyz * 2.0 - 1.0);
+	vec4 vAmbient = gl_LightSource[0].ambient * gl_FrontMaterial.ambient + 
+	     	      	gl_LightModel.ambient * gl_FrontMaterial.ambient;
 
 	float diffuse = max( dot(lVec, bump), 0.0 );
-	
-	vec4 vDiffuse = gl_LightSource[0].diffuse * gl_FrontMaterial.diffuse * 
-					diffuse;	
+
+	vec4 vDiffuse = gl_LightSource[0].diffuse * 
+	     	      	gl_FrontMaterial.diffuse * 
+			diffuse;	
 
 	float specular = pow(clamp(dot(reflect(-lVec, bump), vVec), 0.0, 1.0), 
 	                 gl_FrontMaterial.shininess );
@@ -33,7 +29,9 @@ void main (void)
 	if ( texture2D(colorMap, texCoord).a == 0.0 ){
 		vSpecular = vec4(0,0,0,0); 
 	} else {
-		vSpecular = gl_LightSource[0].specular * gl_FrontMaterial.specular * specular;	
+		vSpecular = gl_LightSource[0].specular * 
+			    gl_FrontMaterial.specular * 
+			    specular;	
 	}
 	gl_FragColor = ( vAmbient*base + vDiffuse*base + vSpecular) * att;
 
--- tests/Resources/Shaders/bump_vertex.glsl
+++ tests/Resources/Shaders/bump_vertex.glsl
@@ -1,7 +1,7 @@
 varying vec3 lightVec; 
 varying vec3 eyeVec;
 varying vec2 texCoord;
-attribute vec3 vTangent; 
+attribute vec4 vTangent; 
 					 
 
 void main(void)
@@ -10,11 +10,11 @@ void main(void)
 	texCoord = gl_MultiTexCoord0.xy;
 	
 	vec3 n = normalize(gl_NormalMatrix * gl_Normal);
-	vec3 t = normalize(gl_NormalMatrix * vTangent);
-	vec3 b = cross(n, t);
+	vec3 t = gl_NormalMatrix * vTangent.xyz;
+	vec3 b = cross(n, t) * vTangent.w;
 	
 	vec3 vVertex = vec3(gl_ModelViewMatrix * gl_Vertex);
-	vec3 tmpVec = gl_LightSource[0].position.xyz - vVertex;
+	vec3 tmpVec = gl_LightSource[0].position.xyz;
 
 	lightVec.x = dot(tmpVec, t);
 	lightVec.y = dot(tmpVec, b);
