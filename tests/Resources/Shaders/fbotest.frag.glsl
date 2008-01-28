fd17a36757f9a16e22be67d79741fb99b303cd28
eNtity <entity@iki.fi>
2008-01-28 10:53:56 +0200

    glowdemo

--- tests/Resources/Shaders/fbotest.frag.glsl
+++ tests/Resources/Shaders/fbotest.frag.glsl
@@ -1,10 +1,10 @@
 uniform sampler2D tex0;
+uniform sampler2D tex1;
 void main()
 {
-	vec4 base = texture2D(tex0, gl_TexCoord[0].xy);
-	vec4 test = base + vec4(0,0,1,0);
-
+	vec4 base  = texture2D(tex0, gl_TexCoord[0].xy);
+	vec4 base2 = texture2D(tex1, gl_TexCoord[1].xy);
 	gl_FragData[0] = base;
-	gl_FragData[1] = test;
+	gl_FragData[1] = base2;
 	
 }
