c5ab5503769d8779e050758cf13bba8eddac8d7c
eNtity <entity@iki.fi>
2008-02-17 10:59:24 +0200

    pcf filtering

--- tests/Resources/Shaders/shadow.frag.glsl
+++ tests/Resources/Shaders/shadow.frag.glsl
@@ -1,9 +1,11 @@
 uniform sampler2DShadow shadowMap;
+uniform sampler2D base;
 varying vec4 projCoord;
 varying vec4 diffuse,ambient;
 varying vec3 normal,lightDir,halfVector;
 	
-	
+const float kTransparency = 0.3;	
+
 void main()
 {
 	vec3 n,halfV;
@@ -25,22 +27,33 @@ void main()
 				pow(NdotHV, gl_FrontMaterial.shininess);
 	}
 
-	const float kTransparency = 0.3;
+	vec3 shadowUV = projCoord.xyz / projCoord.q;
+	float mapScale = 1.0 / 1024.0;
 
+	vec4 shadowColor = shadow2D(shadowMap, shadowUV);
+	shadowColor += shadow2D(shadowMap, shadowUV.xyz + vec3( mapScale,  mapScale, 0));
+	shadowColor += shadow2D(shadowMap, shadowUV.xyz + vec3( mapScale, -mapScale, 0));
+	shadowColor += shadow2D(shadowMap, shadowUV.xyz + vec3( mapScale,  	      0, 0));
+	shadowColor += shadow2D(shadowMap, shadowUV.xyz + vec3(-mapScale,  mapScale, 0));
+	shadowColor += shadow2D(shadowMap, shadowUV.xyz + vec3(-mapScale, -mapScale, 0));
+	shadowColor += shadow2D(shadowMap, shadowUV.xyz + vec3(-mapScale,  	      0, 0));
+	shadowColor += shadow2D(shadowMap, shadowUV.xyz + vec3(        0,  mapScale, 0));
+	shadowColor += shadow2D(shadowMap, shadowUV.xyz + vec3(        0, -mapScale, 0));
 
-	float rValue = shadow2DProj(shadowMap, projCoord).r;
-	rValue = clamp(rValue, 0.0, 1.0);
+	shadowColor = shadowColor / 9.0;
+
+	shadowColor = clamp(shadowColor, 0.0, 1.0);
 	
 	vec3 coordPos  = projCoord.xyz / projCoord.w;
+	vec4 base = texture2D( base, gl_TexCoord[0].st);
 
-	if(coordPos.x >= 0.0 && coordPos.y >= 0.0 && coordPos.x <= 1.0 && coordPos.y <= 1.0 )
+	if(shadowUV.x >= 0.0 && shadowUV.y >= 0.0 && shadowUV.x <= 1.0 && shadowUV.y <= 1.0 )
    	{
-		gl_FragColor = color * rValue;
- 
-	}
+	   gl_FragColor = color * base * shadowColor;
+ 	}
 	else
 	{
-		gl_FragColor = color;
+	   gl_FragColor = base * ambient;
 	}
 	//gl_FragColor = vec4( coordPos.z, coordPos.z,coordPos.z,1.0);
 }
