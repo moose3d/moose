bcf1fb1ff8de7a8c15b6bfc56f7d77da2907b66f
eNtity <entity@iki.fi>
2008-02-17 10:57:32 +0200

    working directional light shadow

--- tests/PhoenixShadowDemo.cpp
+++ tests/PhoenixShadowDemo.cpp
@@ -16,9 +16,12 @@ using namespace Phoenix::Data;
 int g_bLoop = 1;
 #define TEXWIDTH  512
 #define TEXHEIGHT 512
+
 /////////////////////////////////////////////////////////////////
 int main()
 {
+  CSDLScreen::m_SDLScreenParams.m_iWidth = 1024;
+  CSDLScreen::m_SDLScreenParams.m_iHeight = 512;
   if ( CSDLScreen::GetInstance() == NULL )
   {
     cerr << "Can't open screen." << endl;
@@ -28,45 +31,41 @@ int main()
 
   CCamera lightCamera;  
   lightCamera.SetViewport(0,0,TEXWIDTH,TEXHEIGHT);
-  lightCamera.SetNearClipping( -12 );
-  lightCamera.SetFarClipping( 5 );
+  lightCamera.SetNearClipping( -51 );
+  lightCamera.SetFarClipping( 51 );
   lightCamera.SetViewOrtho(-50, 50, -50,50);
   lightCamera.SetPosition(0,0,0);
-  lightCamera.RotateAroundRight( -90 );
+  lightCamera.RotateAroundX( -90 );
   
   CLight directionalLight;
-  directionalLight.SetPosition( 0, 0, 0);
+  directionalLight.SetPosition(0, 0, 0);
   directionalLight.SetType( DIRECTIONAL );
   directionalLight.SetDirection( CVector3<float>( 0,-1,0).GetNormalized());
   directionalLight.SetDiffuseColor( CVector4<unsigned char>(225,225,225,255));
-  directionalLight.SetAmbientColor( CVector4<unsigned char>(125,15,105,255));
+  directionalLight.SetAmbientColor( CVector4<unsigned char>(225,225,225,255));
   directionalLight.SetSpecularColor( CVector4<unsigned char>(255,255,255,255));
-
   //lightCamera.SetRotation( RotationArc( directionalLight.GetDirection(), lightCamera.GetForwardVector() ));
 
   CCamera camera;  
   camera.SetPosition(0,10,10);
-  camera.SetViewport( 0,0, 640, 480 );
+  camera.SetViewport( 0,0, 512, 512 );
   camera.SetNearClipping( 0.1f);
   camera.SetFarClipping( 1000.0f );
   camera.SetFieldOfView( 45.0f);
   camera.RotateAroundUp( 90.0 );
-  CMilkshapeLoader *pLoader = new CMilkshapeLoader() ;
-  pLoader->Load( "Resources/Models/shadowscene.ms3d");
-  pLoader->GenerateModelData();
+
+
+
+
+  CModel *pModel    = Phoenix::Data::LoadMilkshapeModel( "Resources/Models/shadowscene.ms3d",    "shadowmodel");
+
+  COglTexture *pTextureWall = pRenderer->CreateTexture( "./Resources/Textures/wall.tga");
+  assert( pTextureWall  != NULL );
+
+  CModel *pBackdrop = Phoenix::Data::LoadMilkshapeModel( "Resources/Models/shadowbackdrop.ms3d", "shadowbackdrop");
+  g_DefaultTextureManager->Create( pTextureWall, "texture_wall", pBackdrop->GetTextureHandle());
+  pBackdrop->SetShaderParameter( "base", 0 );
   
-  CModel model;
-  g_DefaultVertexManager->Create(pLoader->GetVertices(), "shadowmodel_vertices", model.GetVertexHandle());
-  g_DefaultVertexManager->Create(pLoader->GetNormals(), "shadowmodel_normals", model.GetNormalHandle());
-  
-  model.AddIndexHandle(INDEX_HANDLE());
-  g_DefaultIndexManager->Create( pLoader->GetIndices().front(), "shadowmodel_indices", model.GetIndexHandles().front());
-  
-  pLoader->ResetVertices();
-  pLoader->ResetNormals();
-  pLoader->ResetIndices();
-  
-  delete pLoader;
   SDL_Event event;  
 
   CMaterial material;
@@ -95,8 +94,7 @@ int main()
   CShader *pShadowShader  = pRenderer->CreateShader("Resources/Shaders/shadow.vert.glsl",
 						    "Resources/Shaders/shadow.frag.glsl");
   
-  COglTexture *pTextureTest = pRenderer->CreateTexture( "./Resources/Textures/wall.tga");
-  assert( pTextureTest  != NULL );
+
   
 
   SDL_EnableKeyRepeat(   SDL_DEFAULT_REPEAT_DELAY, SDL_DEFAULT_REPEAT_INTERVAL);
@@ -127,10 +125,45 @@ int main()
 	{
 	  camera.RotateAroundUp( -3.0f);
 	}
+	else if ( event.key.keysym.sym == SDLK_w )
+	{
+	  lightCamera.RotateAroundX( 2.0f);
+	  CQuaternion q = RotationArc(directionalLight.GetDirection(), lightCamera.GetForwardVector());
+	  RotateVector(q, directionalLight.GetDirection() );
+	}
+	else if ( event.key.keysym.sym == SDLK_s )
+	{
+	  lightCamera.RotateAroundX( -2.0f);
+	  CQuaternion q = RotationArc(directionalLight.GetDirection(), lightCamera.GetForwardVector());
+	  RotateVector(q, directionalLight.GetDirection() );
+	}
+	else if ( event.key.keysym.sym == SDLK_a )
+	{
+	  lightCamera.RotateAroundY( -2.0f);
+	  CQuaternion q = RotationArc(directionalLight.GetDirection(), lightCamera.GetForwardVector());
+	  RotateVector(q, directionalLight.GetDirection() );
+	}
+	else if ( event.key.keysym.sym == SDLK_d )
+	{
+	  lightCamera.RotateAroundY( 2.0f);
+	  CQuaternion q = RotationArc(directionalLight.GetDirection(), lightCamera.GetForwardVector());
+	  RotateVector(q, directionalLight.GetDirection() );
+	}
+	else if ( event.key.keysym.sym == SDLK_r )
+	{
+	  lightCamera.Move( CVector3<float>(0,2.0f,0.0) );
+	  directionalLight.Move( CVector3<float>(0,2.0f,0) );
+	}
+	else if ( event.key.keysym.sym == SDLK_f )
+	{
+	  lightCamera.Move( CVector3<float>(0,-2.0f,0));
+	  directionalLight.Move( CVector3<float>(0,-2.0f,0) );
+	}
 
 	break;
       }
     }
+
     if ( camera.IsViewChanged())            camera.UpdateView();
     if ( camera.IsProjectionChanged())      camera.UpdateProjection();
     if ( lightCamera.IsViewChanged())       lightCamera.UpdateView();
@@ -138,37 +171,39 @@ int main()
 
 
 
-    pRenderer->CommitState( STATE_LIGHTING );
+    pRenderer->DisableState( STATE_LIGHTING );
     pRenderer->CommitState( STATE_DEPTH_TEST );
+    pRenderer->CommitState( STATE_FACECULLING );
     // Eliminate artifacts caused by shadow mapping
-    glPolygonOffset(7.0f, 18.0f);
+    glPolygonOffset(18.0f, 1.0f);
     glEnable( GL_POLYGON_OFFSET_FILL );
     
     //pRenderer->CommitFrameBufferSingle( *pFBO, 0);
+      glColorMask( GL_FALSE, GL_FALSE, GL_FALSE, GL_FALSE );
       pRenderer->CommitCamera( lightCamera );
       pRenderer->ClearBuffer(COLOR_BUFFER );
       pRenderer->ClearBuffer(DEPTH_BUFFER );
 
       pRenderer->CommitMaterial( material );
       pRenderer->CommitLight( directionalLight );
-      pRenderer->CommitModel( model  );
+      pRenderer->CommitModel( *pModel  );
 
+    pRenderer->CommitTexture( 0, pTexture );
+    glDisable( GL_POLYGON_OFFSET_FILL );
+    glColorMask( GL_TRUE, GL_TRUE, GL_TRUE, GL_TRUE );
+    glCopyTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, 0, 0, TEXWIDTH, TEXHEIGHT);
+    //pRenderer->RollbackFrameBuffer( *pFBO );      
 
-      pRenderer->CommitTexture( 0, pTexture );
-      glDisable( GL_POLYGON_OFFSET_FILL );
-      //pRenderer->RollbackFrameBuffer( *pFBO );
-      glCopyTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, 0, 0, TEXWIDTH, TEXHEIGHT);
-      
-      //mResult = camera.GetInvView() * lightCamera.GetViewMatrix() * lightCamera.GetProjectionMatrix() * mBias;
-      //mResult = mBias * lightCamera.GetProjectionMatrix() * lightCamera.GetViewMatrix() *camera.GetInvView() ;
-      //mResult.Transpose();
+    //mResult = camera.GetInvView() * lightCamera.GetViewMatrix() * lightCamera.GetProjectionMatrix() * mBias;
+    mResult = mBias * lightCamera.GetProjectionMatrix() * lightCamera.GetViewMatrix() *camera.GetInvView() ;
+    mResult.Transpose();
 
     glMatrixMode(GL_TEXTURE);
-      glLoadMatrixf( mResult.GetArray() );
-       glLoadMatrixf(mBias.GetTransposition().GetArray());		// The bias matrix to convert to a 0 to 1 ratio
-       glMultMatrixf(lightCamera.GetProjectionMatrix().GetTransposition().GetArray());	// The light's projection matrix
-       glMultMatrixf(lightCamera.GetViewMatrix().GetTransposition().GetArray());	// The light's modelview matrix
-       glMultMatrixf(camera.GetInvView().GetTransposition().GetArray());
+           glLoadMatrixf( mResult.GetArray() );
+    //       glLoadMatrixf(mBias.GetTransposition().GetArray());		// The bias matrix to convert to a 0 to 1 ratio
+    //   glMultMatrixf(lightCamera.GetProjectionMatrix().GetTransposition().GetArray());	// The light's projection matrix
+    //  glMultMatrixf(lightCamera.GetViewMatrix().GetTransposition().GetArray());	// The light's modelview matrix
+    //  glMultMatrixf(camera.GetInvView().GetTransposition().GetArray());
     glMatrixMode(GL_MODELVIEW);    
 
     // Here is where we set the mode and function for shadow mapping with shadow2DProj().
@@ -179,19 +214,32 @@ int main()
 
     pRenderer->CommitCamera( camera );    
     pRenderer->CommitShader ( pShadowShader );
-
-       pRenderer->CommitTexture( 0, pTexture );
+       
+       pRenderer->CommitTexture( 7, pTexture );
        pRenderer->CommitFilter( MIN_LINEAR, pTexture->GetType());
        pRenderer->CommitFilter( MAG_LINEAR, pTexture->GetType());
        pRenderer->CommitLight( directionalLight );
-       pRenderer->CommitUniformShaderParam( *pShadowShader, "shadowMap", 0 );
-       pRenderer->CommitModel( model );
-
-    pRenderer->DisableTexture( 0, pTexture );
+       pRenderer->CommitUniformShaderParam( *pShadowShader, "shadowMap", 7 );
+       pRenderer->CommitModel( *pModel );
+       pRenderer->CommitModel( *pBackdrop );
+       pRenderer->DisableTexture(0, pTextureWall );
+    pRenderer->DisableTexture( 7, pTexture );
     pRenderer->CommitShader( NULL );
 
-    pRenderer->Finalize();
+    lightCamera.SetViewport(0,0,TEXWIDTH,TEXHEIGHT);
 
+    // Draw light view into right part of screen
+    lightCamera.SetViewport(512,0,TEXWIDTH, TEXHEIGHT);
+    pRenderer->CommitCamera( lightCamera );
+    
+         pRenderer->ClearBuffer( DEPTH_BUFFER );
+         pRenderer->CommitState( STATE_LIGHTING );
+         pRenderer->CommitMaterial( material );
+         pRenderer->CommitLight( directionalLight );
+         pRenderer->CommitModel( *pModel  );
+       
+    pRenderer->Finalize();
+    lightCamera.SetViewport(0,0,TEXWIDTH, TEXHEIGHT);
     glMatrixMode(GL_TEXTURE);
     glLoadIdentity();
     glMatrixMode(GL_MODELVIEW);    
