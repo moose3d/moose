cbc491787fd7637790cec3e7368d6dc0a83f0068
eNtity <entity@iki.fi>
2008-02-20 17:20:41 +0200

    better way to use helper function

--- src/PhoenixModelHelper.cpp
+++ src/PhoenixModelHelper.cpp
@@ -9,16 +9,13 @@ using namespace Phoenix::Data;
 using std::ostringstream;
 using std::string;
 /////////////////////////////////////////////////////////////////
-Phoenix::Graphics::CModel *
-Phoenix::Data::LoadMilkshapeModel( const char *szFilename, const char *szName )
+int
+Phoenix::Data::LoadMilkshapeModel( const char *szFilename, const char *szName, CModel & rModel, int iFlags  )
 {
 
   if ( szFilename == NULL )	return NULL;
   if ( szName == NULL )		return NULL;
 
-  // Create model
-  CModel *pModel = new CModel();
-  assert( pModel != NULL && "Out of memory, can't create model");
   // Create milkshape loader
   CMilkshapeLoader *pLoader = new CMilkshapeLoader() ;
   assert( pLoader != NULL && "Out of memory, can't create Milkshapeloader");
@@ -27,45 +24,68 @@ Phoenix::Data::LoadMilkshapeModel( const char *szFilename, const char *szName )
   // If loading fails
   if ( pLoader->Load( szFilename ) ) 
   {
-    delete pModel;
     delete pLoader;
     return NULL;
   }
 
   pLoader->GenerateModelData();
+
   ////////////////////
   /// Resource allocation is one-way, either everything succeeds or nothing goes.
-
+  
   // Create vertex handle
   assert( g_DefaultVertexManager->Create(pLoader->GetVertices(), 
 					 name + "_vertices", 
-					 pModel->GetVertexHandle()) == 0);
-  // Create normal handle
-  assert ( g_DefaultVertexManager->Create(pLoader->GetNormals(),  
-					  name + "_normals",  
-					  pModel->GetNormalHandle()) == 0);
-  // Create texcoord handle
-  assert ( g_DefaultVertexManager->Create(pLoader->GetTexCoords(),  
-					  name + "_texcoords0",  
-					  pModel->GetTextureCoordinateHandle()) == 0);
-  // Create index handles 
-  for ( size_t i=0;i<pLoader->GetIndices().size(); i++)
-  {
-    ostringstream stream;  
-    stream << name << "_indices" << i;
-    pModel->AddIndexHandle(INDEX_HANDLE());
-    assert( g_DefaultIndexManager->Create( pLoader->GetIndices()[i], stream.str().c_str(), pModel->GetIndexHandles().back()) == 0 );
-  }
-  
+					 rModel.GetVertexHandle()) == 0);
   // Mark resourcemanager-tracked data so pointers will not be free'd.
   pLoader->ResetVertices();
-  pLoader->ResetNormals();
-  pLoader->ResetTexCoords();
-  pLoader->ResetIndices();
+
+  // load vertex normals
+  if ( iFlags & OPT_VERTEX_NORMALS )
+  {
+    // Create normal handle
+    assert ( g_DefaultVertexManager->Create(pLoader->GetNormals(),  
+					    name + "_normals",  
+					    rModel.GetNormalHandle()) == 0);
+    pLoader->ResetNormals();  
+  }
+  // load texture coordinates
+  if ( iFlags & OPT_VERTEX_TEXCOORDS )
+  {
+    // Create texcoord handle
+    assert ( g_DefaultVertexManager->Create(pLoader->GetTexCoords(),  
+					    name + "_texcoords0",  
+					    rModel.GetTextureCoordinateHandle()) == 0);
+    pLoader->ResetTexCoords();
+  }
+  // load colors
+  if ( iFlags & OPT_VERTEX_COLORS )
+  {
+    // Create texcoord handle
+    assert ( g_DefaultVertexManager->Create(pLoader->GetTexCoords(),  
+					    name + "_texcoords0",  
+					    rModel.GetTextureCoordinateHandle()) == 0);
+    
+  }
+  // Load indices
+  if ( iFlags & OPT_VERTEX_INDICES )
+  {
+    // Create index handles 
+    for ( size_t i=0;i<pLoader->GetIndices().size(); i++)
+    {
+      ostringstream stream;  
+      stream << name << "_indices" << i;
+      pModel->AddIndexHandle(INDEX_HANDLE());
+      assert( g_DefaultIndexManager->Create( pLoader->GetIndices()[i], 
+					     stream.str().c_str(), 
+					     rModel.GetIndexHandles().back()) == 0 );
+    }
+    pLoader->ResetIndices();
+  }
 
   // Release loader memory
   delete pLoader;
-  return pModel;
+  return 0;
 }
 /////////////////////////////////////////////////////////////////
 
--- src/PhoenixModelHelper.h
+++ src/PhoenixModelHelper.h
@@ -7,13 +7,21 @@ namespace Phoenix
 {
   namespace Data
   {
+    enum MS3DDATA_OPTIONS {
+      OPT_VERTEX_NORMALS   = 1 << 0,
+      OPT_VERTEX_COLORS    = 1 << 1,
+      OPT_VERTEX_TEXCOORDS = 1 << 2,
+      OPT_VERTEX_INDICES   = 1 << 3
+    };
     ////////////////////
     /// Loads milkshape file and creates new CModel object out of it.
     /// \param szFilename Path to milkshape model file.
     /// \param szName Model name prefix for storing data in resourcemanagers.
-    /// \returns On success, Pointer to CModel.
-    /// \returns On failure, NULL.
-    Phoenix::Graphics::CModel * LoadMilkshapeModel( const char *szFilename, const char * szName );
+    /// \param rModel CModel where data handles are attached to.
+    /// \param iFlags Flags that indicate what data should be loaded from model - use MS3DDATA_OPTIONS values.
+    /// \returns On success, zero.
+    /// \returns On failure, non-zero.
+    int LoadMilkshapeModel( const char *szFilename, const char * szName, Phoenix::Graphics::CModel & rModel, int iFlags = 0);
   } // namespace Graphics 
 } // namespace Phoenix
 /////////////////////////////////////////////////////////////////
