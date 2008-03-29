4811cb2f91b9ea6a2c90d0172f38904fc3c57556
eNtity <entity@iki.fi>
2008-03-29 13:01:38 +0200

    made more ATI compatible.

--- tests/Resources/Shaders/ps_vertex.glsl
+++ tests/Resources/Shaders/ps_vertex.glsl
@@ -7,7 +7,7 @@ void main()
 	center.y += stuff.w * 1.0 * stuff.y;
 	alpha = stuff.z;
 	gl_Position = gl_ProjectionMatrix * center;
-	gl_TexCoord[0].x = clamp(stuff.x + 1, 0.0, 1.0);
-	gl_TexCoord[0].y = clamp(stuff.y + 1, 0.0, 1.0);
+	gl_TexCoord[0].x = clamp(stuff.x + 1.0, 0.0, 1.0);
+	gl_TexCoord[0].y = clamp(stuff.y + 1.0, 0.0, 1.0);
 }
 
