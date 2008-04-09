be4ea84cbf6444360cb40ffd86cadc43f1f1d2f8
entity <entity@iki.fi>
2008-04-09 15:47:45 +0300

    turning towards enemy and pursuing

--- tests/Resources/Scripts/PlayComputer.tcl
+++ tests/Resources/Scripts/PlayComputer.tcl
@@ -1,6 +1,6 @@
 proc playComputer { } {
 
-    global g_fHealth g_fSpeed g_fMaxSpeed g_fAccel
+    global g_fHealth g_fSpeed g_fMaxSpeed g_fAccel g_bHasTarget
 
     if { $g_fHealth <= 0.0 } {
 
@@ -9,11 +9,18 @@ proc playComputer { } {
 	Rotate 0.0 25 10.0
 
     } else {
+
 	if { $g_fSpeed < 0.010 } {
 	    Accelerate 0.01
 	}
+	
+	if { $g_bHasTarget  } {
+	    TurnTowardsTarget
+	}
+	# if target is OutOfRange  
+	# move towards target.
 	Move
-	Rotate 0.0 128 0.0
+
     }
 
 }
