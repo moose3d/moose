30f94d638d9fa9eff9d5f0191b4c02f8fcfa46db
entity <entity@iki.fi>
2008-04-10 15:04:45 +0300

    advanced scripting

--- tests/Resources/Scripts/PlayComputer.tcl
+++ tests/Resources/Scripts/PlayComputer.tcl
@@ -1,26 +1,51 @@
 proc playComputer { } {
 
-    global g_fHealth g_fSpeed g_fMaxSpeed g_fAccel g_bHasTarget
+    global g_fHealth g_fSpeed g_fMaxSpeed g_fAccel g_bHasTarget \
+	SHIP_ACTION_PASSIVE SHIP_ACTION_MOVING SHIP_ACTION_ATTACKING \
+	SHIP_ACTION_GUARDING SHIP shipActionState
 
     if { $g_fHealth <= 0.0 } {
 
-	#Accelerate -5
-	#Move
-	Rotate 0.0 25 10.0
+	if {  $g_fSpeed > 0.0 } {
+	    Accelerate -0.5
+	    Move
+	}
+
+	Rotate 0.0 2.5 1.0
 
     } else {
 
-	if { $g_fSpeed < 0.010 } {
-	    Accelerate 0.01
-	}
+	switch -- $shipActionState  \
+	$SHIP_ACTION_PASSIVE {
+	    if { $g_fSpeed > 0.0 } {
+		Accelerate -1.5
+		Move
+	    }
+	} \
+	$SHIP_ACTION_MOVING {
+	    if { $g_fSpeed < $g_fMaxSpeed } {
+		Accelerate 0.25
+	    }
 	
-	if { $g_bHasTarget  } {
-	    TurnTowardsTarget
-	}
-	# if target is OutOfRange  
-	# move towards target.
-	Move
+	    if { $g_bHasTarget  } {
+		TurnTowardsTarget
+	    }
+	    # if target is OutOfRange  
+	    # move towards target.
+	    Move
+	    
+	} \
+	$SHIP_ACTION_ATTACKING {
 
-    }
+	} \
+	$SHIP_ACTION_GUARDING {
+
+	} \
+	default {
+
+	}
+    	
+    }    
+    
 
 }
