bc87e854bf1808430ca3112bf36ffbfecd00e414
entity <entity@iki.fi>
2008-05-04 19:35:55 +0300

    rescued script

--- tests/Resources/Scripts/PlayComputer.tcl
+++ tests/Resources/Scripts/PlayComputer.tcl
@@ -1,19 +1,79 @@
+# this is improved script
+proc FullThrust {} {
+    global g_fSpeed g_fMaxSpeed
+
+    if { $g_fSpeed < $g_fMaxSpeed } {
+	Accelerate 0.25
+    }
+}
+
+proc StopShip {} {
+   global g_fSpeed
+    if { $g_fSpeed > 0.0 } {
+	Accelerate -0.5
+    } else {
+	Stop
+    }
+}
 proc playComputer { } {
 
-    global g_fHealth g_fSpeed g_fMaxSpeed g_fAccel
+    global g_fHealth g_fSpeed g_fMaxSpeed g_fAccel g_bHasTarget \
+	SHIP_ACTION_PASSIVE SHIP_ACTION_MOVING SHIP_ACTION_ATTACKING \
+	SHIP_ACTION_GUARDING SHIP shipActionState
 
     if { $g_fHealth <= 0.0 } {
 
-	#Accelerate -5
-	#Move
-	Rotate 0.0 25 10.0
+	if {  $g_fSpeed > 0.0 } {
+	    Accelerate -0.5
+	    Move
+	}
+
+	Rotate 0.0 2.5 1.0
 
     } else {
-	if { $g_fSpeed < 1.0 } {
-	    #Accelerate 5
+
+	switch -- $shipActionState  \
+	$SHIP_ACTION_PASSIVE {
+	    StopShip
+	} \
+	$SHIP_ACTION_MOVING {
+
+	    FullThrust
+	
+	    if { $g_bHasTarget  } {
+		TurnTowardsTarget
+	    }
+	    # if target is OutOfRange  
+	    # move towards target.
+	    Move
+	} \
+	$SHIP_ACTION_ATTACKING {
+
+	    if { [ TargetInWeaponsRange ] == "1" } {
+		if {  [ IsTargetAlive ] } {
+		    StopShip
+		    Move
+		    FirePhasers
+		} else {
+		    NextCommand
+		}
+	    } else {
+
+		if { $g_bHasTarget  } {
+		    TurnTowardsTarget
+		}
+		FullThrust
+		Move
+	    }
+ 	} \
+	$SHIP_ACTION_GUARDING {
+	    
+	} \
+	default {
+
 	}
-	Move
-	Rotate 0.0 50 0.0
-    }
+    	
+    }    
+    
 
 }
