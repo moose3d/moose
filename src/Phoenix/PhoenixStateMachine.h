19dcd241c4b6e3810f13f35f2b2d86d57c66c19f
eNtity <entity@iki.fi>
2008-06-12 18:41:35 +0300

    fixed state machine

--- src/Phoenix/PhoenixStateMachine.h
+++ src/Phoenix/PhoenixStateMachine.h
@@ -7,62 +7,56 @@
 #include <PhoenixResourceManager.h>
 #include <iomanip>
 #include <algorithm>
+#include <iostream>
 /////////////////////////////////////////////////////////////////
 namespace Phoenix
 {
   namespace AI
   {
+    
     /////////////////////////////////////////////////////////////////    
-    template <typename FSM_TYPE, typename STATE_NAME_TYPE, typename INPUT_NAME_TYPE>
-    class CStateMachine;
-    /////////////////////////////////////////////////////////////////    
-    template <typename FSM_TYPE, typename STATE_NAME_TYPE, typename INPUT_NAME_TYPE>
-    class CState : public Phoenix::Core::CGraphNode< FSM_TYPE, STATE_NAME_TYPE, INPUT_NAME_TYPE >
+    template <typename STATE_TYPE>
+    class CState : public Phoenix::Core::CGraphNode,
+		   public Phoenix::Core::CTypeBase<STATE_TYPE>
     {
-      friend class CStateMachine< FSM_TYPE, STATE_NAME_TYPE, INPUT_NAME_TYPE >;
-    public:
-      CState()
-      {
-      }
+    };
+
+    template <typename INPUT_TYPE>
+    class CStateEdge : public Phoenix::Core::CGraphEdge,
+		       public Phoenix::Core::CTypeBase<INPUT_TYPE>
+    {
     };
     ////////////////////
-    /// Finite State Machine. Better implementation would probably be balanced bintree.
-    template <typename FSM_TYPE, typename STATE_NAME_TYPE, typename INPUT_NAME_TYPE>
-    class CStateMachine : public Phoenix::Core::CGraph< FSM_TYPE, STATE_NAME_TYPE, INPUT_NAME_TYPE >
+    /// Finite State Machine. 
+    template <typename STATE_TYPE, typename INPUT_TYPE>
+    class CStateMachine : public Phoenix::Core::CGraph
     {
     protected:
-      size_t m_nNumStates;
-      /// Pointer to all states
-      std::vector< CState<FSM_TYPE, STATE_NAME_TYPE, INPUT_NAME_TYPE> *>  m_vecStates;
+      typedef std::vector< CState< STATE_TYPE> *> StateVector;
+      StateVector m_vecStates;
       ////////////////////
       /// Returns reference to vector of states.
-      inline std::vector< CState<FSM_TYPE, STATE_NAME_TYPE, INPUT_NAME_TYPE> *> & GetStates()
+      inline StateVector & GetStates()
       {
 	return m_vecStates;
       }
       ////////////////////
       /// Current machine state .
-      STATE_NAME_TYPE m_CurrentState;
+      STATE_TYPE m_CurrentState;
     public:
       ////////////////////
       /// Constructor. Creates given number of states for this FSM.
       /// All states are order by STATE_NAME_TYPE
-      CStateMachine( size_t nNumStates) 
+      CStateMachine( size_t nNumStates ) 
       {
-	m_nNumStates = nNumStates;
 	m_vecStates.reserve( nNumStates );
-	SetCurrentState( (STATE_NAME_TYPE)0);
-
+	SetCurrentState( (STATE_TYPE)0 );
+	
 	for(size_t n=0;n<nNumStates;n++)
 	{
-	  // This does not work for some reason - complex template syntax, perhaps?
-	  //m_vecStates[n] = Phoenix::Core::CGraph< FSM_TYPE, STATE_NAME_TYPE, 
-	  // INPUT_NAME_TYPE>::CreateNode< CState< FSM_TYPE, STATE_NAME_TYPE, INPUT_NAME_TYPE> >();
-	  m_vecStates.push_back( new Phoenix::AI::CState<FSM_TYPE, STATE_NAME_TYPE, INPUT_NAME_TYPE>());
-	  Phoenix::Core::CGraph< FSM_TYPE, STATE_NAME_TYPE, INPUT_NAME_TYPE>::m_lstNodes.push_back( m_vecStates[n] );
-	  m_vecStates[n]->m_pGraph = this;
-
-	  m_vecStates[n]->SetName( (STATE_NAME_TYPE)n );
+	  Phoenix::AI::CState<STATE_TYPE> *pState = this->CreateNode< Phoenix::AI::CState<STATE_TYPE> >();
+	  pState->SetType( (STATE_TYPE)n );
+	  m_vecStates.push_back( pState );
 	}
       }
       ////////////////////
@@ -70,25 +64,27 @@ namespace Phoenix
       /// \param nFromState Name of state where transition begins.
       /// \param nToState Name of state where transition leads to.
       /// \param nInput Name of input that ignites transition.
-      void AddTransition( const STATE_NAME_TYPE & nFromState, const STATE_NAME_TYPE &nToState, const INPUT_NAME_TYPE &nInput )
+      void AddTransition( const STATE_TYPE & nFromState, const STATE_TYPE &nToState, const INPUT_TYPE &nInput )
       {
-	Phoenix::Core::CGraph< FSM_TYPE, STATE_NAME_TYPE, INPUT_NAME_TYPE >::AddEdge( m_vecStates[nFromState], m_vecStates[nToState], nInput );
+	Phoenix::Core::CGraphEdge *pEdge = Phoenix::Core::CGraph::AddEdge( m_vecStates[nFromState], m_vecStates[nToState] );
+	static_cast<Phoenix::AI::CStateEdge<INPUT_TYPE> *>(pEdge)->SetType(nInput);
       }
       ////////////////////
       /// Returns name of next state on given input.
       /// \param nState Current state.
       /// \param nInput Input signal.
       /// \returns Name of state following the input.
-      const STATE_NAME_TYPE & StateTransition( const STATE_NAME_TYPE & nState, const INPUT_NAME_TYPE &nInput )
+      const STATE_TYPE & StateTransition( const STATE_TYPE & nState, const INPUT_TYPE &nInput )
       {
-	typename std::list< Phoenix::Core::CGraphEdge< FSM_TYPE, STATE_NAME_TYPE, INPUT_NAME_TYPE > *>::iterator it;
+	std::list< Phoenix::Core::CGraphEdge *>::iterator it;
 	it = GetStates()[nState]->GetLeavingEdges().begin();
 
 	for( ; it != GetStates()[nState]->GetLeavingEdges().end(); it++)
 	{
-	  if ( (*it)->GetName() == nInput )
+
+	  if ( static_cast< Phoenix::AI::CStateEdge<INPUT_TYPE> *>(*it)->GetType() == nInput )
 	  {
-	    return (*it)->GetToNode()->GetName();
+	    return static_cast< Phoenix::AI::CState<STATE_TYPE> *>((*it)->GetToNode())->GetType();
 	  }
 	}
 	return nState;
@@ -96,21 +92,21 @@ namespace Phoenix
       ///////////////////
       /// Processes given input and sets state accordingly.
       /// \param nInput Input where state machine reacts.
-      void Process( const INPUT_NAME_TYPE & nInput )
+      void Process( const INPUT_TYPE & nInput )
       {
 	SetCurrentState( StateTransition( GetCurrentState(), nInput));
       }
       ////////////////////
       /// Sets current state.
       /// \param state Next state.
-      void SetCurrentState( const STATE_NAME_TYPE & state )
+      void SetCurrentState( const STATE_TYPE & state )
       {
 	m_CurrentState = state;
       }
       ////////////////////
       /// Returns current state.
       /// \returns Current state.
-      const STATE_NAME_TYPE & GetCurrentState()
+      const STATE_TYPE & GetCurrentState()
       {
 	return m_CurrentState;
       }
@@ -118,7 +114,7 @@ namespace Phoenix
       /// Comparison operator for states directly.
       /// \param state State which current state is compared against.
       /// \returns Non-zero, if states are equal - zero otherwise.
-      int operator==( const STATE_NAME_TYPE & state )
+      bool operator==( const STATE_TYPE & state )
       {
 	return ( GetCurrentState() == state );
       }
