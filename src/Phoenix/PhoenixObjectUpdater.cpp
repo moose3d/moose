3b0d439674152a9082f4e5b6f585d5dc5e136810
entity <entity@iki.fi>
2008-06-19 15:44:00 +0300

    objectupdater impl

---
+++ src/Phoenix/PhoenixObjectUpdater.cpp
@@ -0,0 +1,29 @@
+#include "PhoenixObjectUpdater.h"
+/////////////////////////////////////////////////////////////////
+Phoenix::Core::CObjectUpdater::~CObjectUpdater()
+{
+  for(size_t n=0;n<m_vecUpdateables.size();n++)
+  {
+    delete m_vecUpdateables[n];
+  }
+  m_vecUpdateables.clear();
+}
+/////////////////////////////////////////////////////////////////
+void 
+Phoenix::Core::CObjectUpdater::Update( size_t nTimeMS )
+{
+  UpdateableVector::iterator it = m_vecUpdateables.begin();
+  for( ; it != m_vecUpdateables.end();it++)
+  {
+    if ( (*it)->IsNull())
+    {
+      delete (*it);
+      m_vecUpdateables.erase(it);
+    } 
+    else
+    {
+      (*it)->Update( nTimeMS);
+    }
+  } 
+}
+/////////////////////////////////////////////////////////////////
