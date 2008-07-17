e7ca571017f803ca64392d6c3d9962464df74bdc
eNtity <entity@iki.fi>
2008-07-17 11:40:32 +0300

    TEX_3F type added

--- src/Phoenix/PhoenixVertexDescriptor.cpp
+++ src/Phoenix/PhoenixVertexDescriptor.cpp
@@ -10,73 +10,52 @@ Phoenix::Graphics::CVertexDescriptor::CVertexDescriptor( ELEMENT_TYPE nType,
   case ELEMENT_TYPE_VERTEX_3F:
   case ELEMENT_TYPE_NORMAL_3F:
   case ELEMENT_TYPE_COLOR_3F:
+  case ELEMENT_TYPE_UNIFORM_3F:
+  case ELEMENT_TYPE_TEX_3F:
+  case ELEMENT_TYPE_ATTRIB_3F:
     m_pData = new float[3*nNumElements];
     break;
   case ELEMENT_TYPE_TEX_2F:
-    m_pData = new float[2*nNumElements];
-    break;
-  case ELEMENT_TYPE_COLOR_4UB:
-    m_pData = new unsigned char[4*nNumElements];
-    break;
-  case ELEMENT_TYPE_COLOR_4F:
-    m_pData = new float[4*nNumElements];
-    break;
-  case ELEMENT_TYPE_UNIFORM_1F:
-    m_pData = new float[1*nNumElements];
-    break;
+  case ELEMENT_TYPE_ATTRIB_2F:
   case ELEMENT_TYPE_UNIFORM_2F:
     m_pData = new float[2*nNumElements];
     break;
-  case ELEMENT_TYPE_UNIFORM_3F:
-    m_pData = new float[3*nNumElements];
+  case ELEMENT_TYPE_COLOR_4UB:
+  case ELEMENT_TYPE_ATTRIB_4UB:
+    m_pData = new unsigned char[4*nNumElements];
     break;
+  case ELEMENT_TYPE_COLOR_4F:
   case ELEMENT_TYPE_UNIFORM_4F:
+  case ELEMENT_TYPE_ATTRIB_4F:
+  case ELEMENT_TYPE_UNIFORM_2X2F:
     m_pData = new float[4*nNumElements];
     break;
+  case ELEMENT_TYPE_UNIFORM_1F:
+  case ELEMENT_TYPE_ATTRIB_1F:
+    m_pData = new float[nNumElements];
+    break;
   case ELEMENT_TYPE_UNIFORM_1I:
-    m_pData = new int[1*nNumElements];
+  case ELEMENT_TYPE_ATTRIB_1I:
+    m_pData = new int[nNumElements];
     break;
   case ELEMENT_TYPE_UNIFORM_2I:
+  case ELEMENT_TYPE_ATTRIB_2I:
     m_pData = new int[2*nNumElements];
     break;
   case ELEMENT_TYPE_UNIFORM_3I:
+  case ELEMENT_TYPE_ATTRIB_3I:
     m_pData = new int[3*nNumElements];
     break;
+
   case ELEMENT_TYPE_UNIFORM_4I:
+  case ELEMENT_TYPE_ATTRIB_4I:
     m_pData = new int[4*nNumElements];
-    break;
-  case ELEMENT_TYPE_UNIFORM_2X2F:
-    m_pData = new float[4*nNumElements];
     break;
   case ELEMENT_TYPE_UNIFORM_3X3F:
     m_pData = new float[9*nNumElements];
     break;
   case ELEMENT_TYPE_UNIFORM_4X4F:
     m_pData = new float[16*nNumElements];
-    break;
-  case ELEMENT_TYPE_ATTRIB_1F:
-    m_pData = new float[nNumElements];
-    break;
-  case ELEMENT_TYPE_ATTRIB_2F:
-    m_pData = new float[2*nNumElements];
-    break;
-  case ELEMENT_TYPE_ATTRIB_3F:
-    m_pData = new float[3*nNumElements];
-    break;
-  case ELEMENT_TYPE_ATTRIB_4F:
-    m_pData = new float[4*nNumElements];
-    break;
-  case ELEMENT_TYPE_ATTRIB_1I:
-    m_pData = new int[nNumElements];
-    break;
-  case ELEMENT_TYPE_ATTRIB_2I:
-    m_pData = new int[2*nNumElements];
-    break;
-  case ELEMENT_TYPE_ATTRIB_3I:
-    m_pData = new int[3*nNumElements];
-    break;
-  case ELEMENT_TYPE_ATTRIB_4I:
-    m_pData = new int[4*nNumElements];
     break;
   case ELEMENT_TYPE_ATTRIB_1UB:
     m_pData = new unsigned char[nNumElements];
@@ -86,9 +65,6 @@ Phoenix::Graphics::CVertexDescriptor::CVertexDescriptor( ELEMENT_TYPE nType,
     break;
   case ELEMENT_TYPE_ATTRIB_3UB:
     m_pData = new unsigned char[3*nNumElements];
-    break;
-  case ELEMENT_TYPE_ATTRIB_4UB:
-    m_pData = new unsigned char[4*nNumElements];
     break;
   case ELEMENT_TYPE_NULL:
     m_pData = NULL;
@@ -116,6 +92,7 @@ Phoenix::Graphics::CVertexDescriptor::~CVertexDescriptor()
   case ELEMENT_TYPE_COLOR_3F:
   case ELEMENT_TYPE_COLOR_4F:
   case ELEMENT_TYPE_NORMAL_3F:
+  case ELEMENT_TYPE_TEX_3F:
     delete reinterpret_cast<float *>(m_pData);
     break;
   case ELEMENT_TYPE_COLOR_4UB:
