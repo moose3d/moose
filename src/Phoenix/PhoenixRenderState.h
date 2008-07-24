cbc9c90cd80bb1855966b7ab5fac84904c80f7f3
eNtity <entity@iki.fi>
2008-07-24 13:37:57 +0300

    added basecolor (default white)

--- src/Phoenix/PhoenixRenderState.h
+++ src/Phoenix/PhoenixRenderState.h
@@ -20,13 +20,16 @@ namespace Phoenix
       bool	     m_DepthTest;
       bool	     m_DepthWrite;
       bool	     m_FaceCulling;
+      Phoenix::Math::CVector4<unsigned char> m_BaseColor;
     public:      
+      CRenderState() : m_BaseColor(255,255,255,255) {}
       inline Phoenix::Graphics::CAlphaTestOperation & GetAlphaOperation()    { return m_AlphaTestOp; }
       inline Phoenix::Graphics::CBlendingOperation  & GetBlendingOperation() { return m_BlendingOp;  }
       inline bool IsTransparent() const    { return m_BlendingOp.IsEnabled(); }
       inline bool & GetDepthTest()  { return m_DepthTest; }
       inline bool & GetDepthWrite() { return m_DepthWrite; }
       inline bool & GetFaceCulling() { return m_FaceCulling; }
+      inline CVector4<unsigned char> & GetBaseColor() { return m_BaseColor; }
     };
   }
 }
