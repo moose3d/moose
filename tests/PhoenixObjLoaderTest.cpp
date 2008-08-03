cc70826bf8e324e51b6641f87348bcfd333752d4
eNtity <entity@iki.fi>
2008-08-03 18:04:03 +0300

    new tests

--- tests/PhoenixObjLoaderTest.cpp
+++ tests/PhoenixObjLoaderTest.cpp
@@ -4,10 +4,32 @@
 #include <iostream>
 /////////////////////////////////////////////////////////////////
 using namespace Phoenix::Data;
+using namespace Phoenix::Graphics;
+using namespace std;
 /////////////////////////////////////////////////////////////////
 TEST( PhoenixObjLoader )
 {
-  CObjLoader loader;
-  CHECK_EQUAL( 0, loader.Load( "box.obj" ));
+  CModelLoader *pLoader = new CObjLoader();
+  CHECK_EQUAL( 0, pLoader->Load( "box.obj" ) ); 
+  CVertexDescriptor *pVertices = pLoader->GetVertexArray();
+  cerr << "# vertices loaded: " << pVertices->GetSize() << endl;
+  CIndexArray *pIndices = pLoader->GetIndexArray();
+  cerr << "# indices : " << pIndices->GetNumIndices() << endl;
+  delete pIndices;
+
+  pIndices = pLoader->GetIndexArray( "testgroup1");
+  cerr << "# indices in testgroup1: " << pIndices->GetNumIndices() << endl;
+  delete pIndices;
+
+  pIndices = pLoader->GetIndexArray( "testgroup2");
+  cerr << "# indices in testgroup2: " << pIndices->GetNumIndices() << endl;
+  delete pIndices;
+
+  pIndices = pLoader->GetIndexArray( "testgroup3");
+  cerr << "# indices in testgroup3: " << pIndices->GetNumIndices() << endl;
+  delete pIndices;
+
+  delete pVertices;
+  delete pLoader;
 }
 /////////////////////////////////////////////////////////////////
