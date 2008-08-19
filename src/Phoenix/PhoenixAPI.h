d755413b512cfdf06686b8bf5ff534f2ab28ea9b
eNtity <entity@iki.fi>
2008-08-19 12:32:20 +0300

    added apidef

---
+++ src/Phoenix/PhoenixAPI.h
@@ -0,0 +1,14 @@
+#ifndef __PhoenixAPI_h__
+#define __PhoenixAPI_h__
+/////////////////////////////////////////////////////////////////// 
+// windows needs these for DLLs
+// while compiling DLL library, define DLLEXPORT.
+# if defined(DLLEXPORT) 
+#   define PHOENIX_API __declspec(dllexport)
+# else 
+#   define PHOENIX_API __declspec(dllimport)
+# endif
+#else
+# define PHOENIX_API  /* Unix does not need this */
+/////////////////////////////////////////////////////////////////
+#endif
