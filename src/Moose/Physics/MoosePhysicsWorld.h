9c01887359e5adee9b842d842fa4f982a31962b1
eNtity <entity@eNtitys-MacBook-Pro.local>
2010-11-29 18:00:59 +0200

    moose physics world

---
+++ src/Moose/Physics/MoosePhysicsWorld.h
@@ -0,0 +1,91 @@
+////////////////////////////////////////////////////////////////////////////////
+#include <ode/ode.h>
+////////////////////////////////////////////////////////////////////////////////
+namespace Moose
+{
+  namespace Physics 
+  {
+    class CPhysicsWorld
+    {
+    protected:
+      dWorldID m_WorldID;
+      bool     m_bQuickStep; ///!< Is quickstep algorithm enabled.
+      
+    public:
+      /// Creates world.
+      CPhysicsWorld();
+      /// Destroys world.
+      virtual ~CPhysicsWorld();
+      /// Sets gravity for world.
+      /// \param vGravity Gravity to be set.
+      void SetGravity( const Moose::Math::CVector3<float> & vGravity );
+      /// \returns Gravity for this world.
+      const Moose::Math::CVector3<float> & GetGravity();
+      /// Sets error correction per step. (commonly 0.1- 0.8)
+      /// \param fErp Error correctionm value.
+      void SetERP( float fErp );
+      /// \returns Error correction per step.
+      float GetERP();
+      
+      ////////////////////
+      /// Sets constraint for mixing value (10^-9 - 1)
+      /// \param fCfm CFM mixing value to be set.
+      void SetConstraintForceMixing( float fCfm );
+      ////////////////////
+      /// Returns Constraint force mixing value.
+      float GetConstraintForceMixing();
+      void SetAutoDisableFlag( bool bFlag );
+      bool GetAutoDisableFlag();
+      
+      void SetAutoDisableLinearThreshold( float fValue);
+      float GetAutoDisableLinearThreshold();
+
+      void SetAutoDisableAngularThreshold( float fValue );
+      float GetAutoDisableAngluarThreshold();
+      
+      void SetAutoDisableSteps( int iSteps );
+      int GetAutoDisableSteps();
+
+      void SetAutoDisableTime( float fTime );
+      float GetAutoDisableTime();
+
+      void ImpulseToForce( float fStepSize, float fX, float fY, float fZ, 
+                           CVector3<float> & vVector );
+      
+      void FixedUpdate( float fStepSize );
+
+      void UseQuickStep( bool bFlag );
+      
+      void SetQuickStepIterations( size_t nNumIterations );
+      size_t GetQuickStepIterations();
+      
+      void SetQuickStepOverRelaxation( float fOverRelaxation );
+      float GetQuickStepOverRelaxation( );
+      
+      void SetLinearDamping( float fScale );
+      float GetLinearDamping();
+      
+      void SetAngularDamping( float fScale );
+      float GetAngularDamping( );      
+      
+      void SetLinearDampingThreshold( float fThreshold );
+      float GetLinearDampingThreshold( );
+
+      void SetAngularDampingThreshold( float fThreshold );
+      float GetAngularDampingThreshold( );
+
+      void SetMaxAngularSpeed( float fMaxSpeed );
+      float GetMaxAngularSpeed();
+
+      void SetContactMaxCorrectingVelocity( float fVel );
+      float GetContactMaxCorrectingVelocity();
+
+      void SetContactSurfaceLayer( float fDepth );
+      float GetContactSurfaceLayer();
+
+
+
+      
+    };
+  } // Physics
+} // Moose
---
+++ src/Moose/Physics/MooseRigidBody.h
@@ -0,0 +1,131 @@
+////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////
+namespace Moose
+{
+  namespace Physics 
+  {
+    class CRigidBody 
+    {
+    public:
+      enum RotationMode  ///!< Rotation mode for body
+        { 
+          kInfiniteSimal, ///!< Faster to computer, more error-prone.
+          kFinite         ///!< Slower to compute, might help in some situations.
+        } ;
+    protected:
+      dBodyID m_BodyID; ///!< ODE Body id.
+    public:
+      /// Creates body.
+      CRigidBody();
+      /// Destroys body.
+      virtual ~CRigidBody();
+
+      void SetPosition( const Moose::Math.:CVector3<float> & vPosition );
+      const Moose::Math::CVector3<float> GetPosition();
+
+      void SetRotation( const Moose::Math::CQuaternion & q );
+      Moose::Math::CQuaternion GetRotation();
+      
+      void SetVelocity( const Moose::Math::CVector3<float> & vVelocity );
+      Moose::Math::CVector3<float> GetVelocity();
+      
+      void SetAngularVelocity( const Moose::Math::CVector3<float> & vVelocity );
+      Moose::Math::CVector3<float> GetAngularVelocity(); 
+
+      void SetMass( float fMass );
+      float GetMass();
+
+      void AddForce( const Moose::Math::CVector3<float> & vForce );
+      void AddTorque( const Moose::Math::CVector3<float> & vForce );
+      void AddRelativeTorque( const Moose::Math::CVector3<float> & vForce );
+      void AddRelativeForce( const Moose::Math::CVector3<float> & vForce );
+
+      void AddForceAtPos( const Moose::Math::CVector3<float> & vForce, 
+                          const Moose::Math::CVector3<float> & vPos );
+      void AddForceAtRelPos( const Moose::Math::CVector3<float> & vForce, 
+                             const Moose::Math::CVector3<float> & vPos );
+      void AddRelForceAtPos( const Moose::Math::CVector3<float> & vForce, 
+                          const Moose::Math::CVector3<float> & vPos );
+      void AddRelForceAtRelPos( const Moose::Math::CVector3<float> & vForce, 
+                                const Moose::Math::CVector3<float> & vPos );
+      Moose::Math::CVector3<float> GetForce();
+      Moose::Math::CVector3<float> GetTorque();
+
+      void GetForce( const Moose::Math::CVector3<float> & vForce );
+      void GetTorque( const Moose::Math::CVector3<float> & vTorque );
+
+      void SetKinematic ( bool bFlag );
+      bool IsKinematic();
+      
+      void SetEnabled( bool bFlag );
+      bool IsEnabled();
+
+      void SetAutoDisable( bool bFlag );
+      bool IsAutoDisable();
+
+      /// Set dInfinity to disable.
+      void SetAutoDisableLinearThreshold( float fThreshold );
+      float GetAutoDisableLinearThreshold();
+
+      /// Set dInfinity to disable.
+      void SetAutoDisableAngularThreshold( float fThreshold );
+      float GetAutoDisableAngularThreshold();
+      
+      /// Set to zero to disable.
+      void SetAutoDisableSteps( size_t nSteps );
+      size_t GetAutoDisableSteps();
+      
+      /// Set to zero to disable.
+      void SetAutoDisableTime( float fTime );
+      float GetAutoDisableTime();
+
+      /// Sets autodisable parameters that are set on the world object.
+      void SetAutoDisableDefaults();
+      void SetLinearDamping( float fScale );
+      void SetAngularDamping( float fScale );
+      float GetLinearDamping();
+      float GetAngularDamping();
+      
+      float GetLinearDampingThreshold();
+      float GetAngularDampingThreshold();
+      void SetLinearDampingThreshold( float fThreshold );
+      void SetAngularDampingThreshold( float fThreshold );
+
+      /// Sets world damping values to this object.
+      void SetDampingDefaults();
+      // By default, dInfinity
+      void SetMaxAngularSpeed( float fMaxSpeed );
+      float GetMaxAngularSpeed();
+
+      void RegisterWithGameObject( Moose::Scene::CGameObject *pGameObject );
+      // dBodySetMovedCallsback      
+      // dBodySetData dBodyGetData
+      
+      void SetFiniteRotationMode( RotationMode kMode );
+      RotationMode GetFiniteRotationMode();
+      void SetFiniteRotationAxis( Moose::Math::CVector3<float> & vAxis );
+      Moose::Math::CVector3<float> GetFiniteRotationAxis();
+      size_t GetNumJoints();
+      // dJointId GetJoint( size_t index );
+      // dWorldId GetWorld();
+      /// Does rigidbody use gravity or not.
+      /// \param bFlag True to use gravity, false to not.
+      void UseGravity( bool bFlag );
+      /// \returns true if body uses world gravity, false otherwise. 
+      bool UsesGravity();
+
+      // dGeomId dBodyGetFirstGeom
+      // dGeomId dBodyGetNextGeom gGeomId
+      
+      ////////////////////
+      /// Called when body moves.
+      inline static void OnMoveTrigger( dBodyId id );
+
+      void UseSphere( Moose::Volume::CSphere & sphere );
+      //      void Physics
+    };
+  } // UseBox
+} // Moose
+
+
