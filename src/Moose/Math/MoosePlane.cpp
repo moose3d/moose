6e70b1145564893b28cfcd5038a7fedbc3549629
eNtity <entity@iki.fi>
2013-01-27 20:21:44 +0200

    added copyright notices

--- src/Moose/Math/MoosePlane.cpp
+++ src/Moose/Math/MoosePlane.cpp
@@ -1,3 +1,24 @@
+/******************************************************************************/
+ * Moose3D is a game development framework.
+ *
+ * Copyright 2006-2013 Anssi Gröhn / entity at iki dot fi.
+ *
+ * This file is part of Moose3D.
+ *
+ * Moose3D is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Moose3D is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Moose3D.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+
 #include "MoosePlane.h"
 #include "MooseMath.h"
 #include "MooseVector4.h"
